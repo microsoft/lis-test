12fd423548ad5f575be15378d230075504ada8d0
sixiao-ms <simonseattle@outlook.com>
2014-03-07 14:18:34 -0800

    Add infrastructure scripts for Lisa
    Adding 4 powershell scripts to enable the LISA end to end test
    automation.

---
+++ WS2012R2/lisa/Infrastructure/Parse-LisaResultAsXmlLog.ps1
@@ -0,0 +1,159 @@
+########################################################################
+#
+# Linux on Hyper-V and Azure Test Code, ver. 1.0.0
+# Copyright (c) Microsoft Corporation
+#
+# All rights reserved. 
+# Licensed under the Apache License, Version 2.0 (the ""License"");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0  
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+# ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR
+# PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+#
+# See the Apache Version 2.0 License for specific language governing
+# permissions and limitations under the License.
+#
+########################################################################
+
+
+
+<#
+.Synopsis
+    Parse the LISA test log and create a simple XML log file.
+
+.Description
+    Parse the LISA test log (ica.log by default), create a simple XML log, and copy it to a test harness running folder.
+    
+.Parameter LisLogFileName
+    The log file produced by LISA. By default it is named ica.log. 
+
+.Parameter XMLLogFileName
+    The XML log file name to be created.
+
+.Parameter LogFolder
+    A folder to save the script running logs.
+
+.Parameter TestRunWorkingDir
+    The test harness folder which needs the XML log file.
+
+.Exmple
+    Parse-LisaResultAsXmlLog.ps1 ica.log \\AB803750-2129-436B-AFE6-02F912A3155C.xml D:\Logs F:\TestRunWorkingDir
+
+#>
+
+param( [string]$LisLogFileName = "ica.log", [string]$XMLLogFileName, [string]$LogFolder, [string]$TestRunWorkingDir )
+
+#----------------------------------------------------------------------------
+# Start a new PowerShell log.
+#----------------------------------------------------------------------------
+Start-Transcript "$LogFolder\Parse-LisaResultAsXmlLog.ps1.log" -force
+
+#----------------------------------------------------------------------------
+# Print running information
+#----------------------------------------------------------------------------
+Write-Host "Running [Parse-LisaResultAsXmlLog.ps1]..." -foregroundcolor cyan
+Write-Host "`$LisLogFileName   = $LisLogFileName" 
+Write-Host "`$XMLLogFileName   = $XMLLogFileName" 
+Write-Host "`$LogFolder        = $LogFolder" 
+Write-Host "`$TestRunWorkingDir = $TestRunWorkingDir" 
+
+#----------------------------------------------------------------------------
+# Verify required parameters
+#----------------------------------------------------------------------------
+if ($LisLogFileName -eq $null -or $LisLogFileName -eq "")
+{
+    Throw "Parameter LisLogFileName is required."
+}
+if ($XMLLogFileName -eq $null -or $XMLLogFileName -eq "")
+{
+    Throw "Parameter XMLLogFileName is required."
+}
+if ($LogFolder -eq $null -or $LogFolder -eq "")
+{
+    Throw "Parameter LogFolder is required."
+}
+if ($TestRunWorkingDir -eq $null -or $TestRunWorkingDir -eq "")
+{
+    Throw "Parameter TestRunWorkingDir is required."
+}
+
+
+#----------------------------------------------------------------------------
+# Read the Lisa log file
+#----------------------------------------------------------------------------
+$numberOfPassed = 0
+$numberOfFailed = 0
+$icaLogs = Get-ChildItem "$LogFolder\$LisLogFileName" -Recurse
+Write-Host "Number of Log files found: "
+Write-Host $icaLogs.Count
+
+foreach ($logFile  in $icaLogs)
+{
+    Write-Host "One log file has been found: $logFile" 
+
+    $logContent = get-content $logFile
+	$beginToFind = $false
+	foreach ($line in $logContent)
+	{
+	    if ($line -eq "Test Results Summary")
+		{
+		    $beginToFind = $true
+             Write-Host "Starting to look for the test result from the log file..."   
+		}
+		if ($beginToFind -eq $true)
+		{
+            Write-host $line
+		    if ($line.Trim().StartsWith("Test") -and $line.Contains(":") ) 
+            {
+                if ($line.Contains("Success"))
+                {
+                    $numberOfPassed ++
+                    Write-Host ">>>>> One test passed"   
+                }
+                else
+                {
+                    $numberOfFailed ++
+                    Write-Host "!!!!! One test failed"   
+                }
+            }
+		}
+	}
+}
+$totalTests = $numberOfPassed + $numberOfFailed
+
+
+#----------------------------------------------------------------------------
+# Save it to the XML file format
+#----------------------------------------------------------------------------
+# Set the File Name
+$XMLLogFilePath = "$LogFolder\$XMLLogFileName"
+Write-Host "Starting create XML Log file: $XMLLogFilePath " 
+
+# Create The Document
+$XmlWriter = New-Object System.XMl.XmlTextWriter($XMLLogFilePath,$Null)
+ 
+$xmlWriter.WriteStartElement("TaskResult")
+$xmlWriter.WriteAttributeString("Total", $totalTests)
+$xmlWriter.WriteAttributeString("Pass", $numberOfPassed)
+$xmlWriter.WriteAttributeString("Failed", $numberOfFailed)
+$xmlWriter.WriteAttributeString("Blocked", "0")
+$xmlWriter.WriteAttributeString("Warned", "0")
+$xmlWriter.WriteAttributeString("Skipped", "0")
+$xmlWriter.WriteEndElement()
+				
+# Finish The Document
+$xmlWriter.Finalize
+$xmlWriter.Flush
+$xmlWriter.Close()
+
+Copy-Item $XMLLogFilePath $TestRunWorkingDir
+
+Write-Host "XML Log file has been created: $XMLLogFilePath " 
+Write-Host "Running [Parse-LisaResultAsXmlLog] FINISHED (NOT VERIFIED)."
+
+Stop-Transcript
+exit 0
---
+++ WS2012R2/lisa/Infrastructure/Parse-XmlForGlobalParams.ps1
@@ -0,0 +1,112 @@
+########################################################################
+#
+# Linux on Hyper-V and Azure Test Code, ver. 1.0.0
+# Copyright (c) Microsoft Corporation
+#
+# All rights reserved. 
+# Licensed under the Apache License, Version 2.0 (the ""License"");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0  
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+# ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR
+# PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+#
+# See the Apache Version 2.0 License for specific language governing
+# permissions and limitations under the License.
+#
+########################################################################
+
+
+
+<#
+.Synopsis
+    Parse the LISA XML file.
+
+.Description
+    Parse the LISA XML file and register the global test parameters to the test client.
+    This script needs command support of WttCmd. Please don't run this script if you don't have WttCmd installed in the test client. 
+    
+.Parameter XmlFilename
+    The LISA XML file which defined the LISA test. 
+
+.Exmple
+    Parse-XmlForGlobalParams.ps1 SimpleLisaTest.xml
+
+#>
+param( [string]$XmlFilename )
+
+#----------------------------------------------------------------------------
+# Start a new PowerShell log.
+#----------------------------------------------------------------------------
+Start-Transcript "Parse-XmlForGlobalParams.ps1.log" -force
+
+#----------------------------------------------------------------------------
+# Print running information
+#----------------------------------------------------------------------------
+Write-Host "Running [Parse-XmlForGlobalParams.ps1]..." -foregroundcolor cyan
+Write-Host "`$XmlFilename    = $XmlFilename" 
+
+#----------------------------------------------------------------------------
+# Verify XML file and read it
+#----------------------------------------------------------------------------
+if ($XmlFilename -eq $null -or $XmlFilename -eq "")
+{
+    Throw "Parameter XmlFilename is required."
+}
+else
+{
+    if (! (test-path $XmlFilename))
+    {
+        write-host -f Red "Error: XML config file '$XmlFilename' does not exist."
+        Throw "Parameter XmlFilename is required."
+    }
+}
+
+$xmlConfig = [xml] (Get-Content -Path $xmlFilename)
+if ($null -eq $xmlConfig)
+{
+    write-host -f Red "Error: Unable to parse the .xml file"
+    return $false
+}
+
+#----------------------------------------------------------------------------
+# Get the VM defined in the XML file
+#----------------------------------------------------------------------------
+$numberOfVMs = $xmlConfig.config.VMs.ChildNodes.Count
+Write-Host "Number of VMs defined in the XML file: $numberOfVMs"
+
+if ($numberOfVMs -eq 0)
+{
+    Throw "No VM is defined in the LISA XML file."
+}
+elseif ($numberOfVMs -gt 1)
+{
+    $VMName = $xmlConfig.config.VMs.VM.VMName[0]
+}
+else
+{
+    $VMName = $xmlConfig.config.VMs.VM.VMName
+}
+Write-Host "The VM will be used as SUT for this testing: $VMName"
+
+#----------------------------------------------------------------------------
+# Get the Log Folder defined in the XML file
+#----------------------------------------------------------------------------
+$logDir = $xmlConfig.config.global.logfileRootDir
+Write-Host "Log Folder defined in the XML file: $logDir"
+
+#----------------------------------------------------------------------------
+# Write the value to WTT Client's SysInitKey
+#----------------------------------------------------------------------------
+cmd /c "wttcmd.exe /SysInitKey /Key:VMNameBeingTested /Value:$VMName"
+cmd /c "wttcmd.exe /SysInitKey /Key:LocalTestLogDir /Value:$logDir"
+
+Write-Host "Params have been set to the WTT Client machine by WttCmd."
+
+Write-Host "Running [Parse-XmlForGlobalParams.ps1] FINISHED (NOT VERIFIED)."
+
+Stop-Transcript
+exit 
