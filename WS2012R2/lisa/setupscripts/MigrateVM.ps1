e5096adaa3fb2df01ece17954defea2a26013a87
Nesa Ion-Claudiu <claudiu.nesa@gmail.com>
2014-05-23 12:54:13 +0300

    Added Apache license and script description

---
+++ WS2012R2/lisa/setupscripts/MigrateVM.ps1
@@ -0,0 +1,38 @@
+########################################################################
+#
+# Linux on Hyper-V and Azure Test Code, ver. 1.0.0
+# Copyright (c) Microsoft Corporation
+#
+# All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the ""License"");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+# ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR
+# PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+#
+# See the Apache Version 2.0 License for specific language governing
+# permissions and limitations under the License.
+#
+########################################################################
+<#
+.Synopsis
+    Performs basic Live/Quick Migration operations
+.Description
+    This is a Powershell script that migrates a VM from one cluster node
+    to another.
+    The script assumes that the second node is configured
+.Parameter vmName
+    Name of the VM to migrate.
+.Parameter hvServer
+    Name of the Hyper-V server hosting the VM.
+.Parameter migrationType
+    Type of the migration to perform
+.Example
+
+.Link
+    None.
+#>
