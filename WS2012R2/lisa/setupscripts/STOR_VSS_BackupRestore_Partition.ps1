f1772b1afab77d3a07d98cf7e400f9d8ba3259d4
Dorin Paslaru <dpaslaru@cloudbasesolutions.com>
2014-06-11 21:42:26 +0300

    Update STOR_VSS_BackupRestore_Partition.ps1

--- WS2012R2/lisa/setupscripts/STOR_VSS_BackupRestore_Partition.ps1
+++ WS2012R2/lisa/setupscripts/STOR_VSS_BackupRestore_Partition.ps1
@@ -312,7 +312,7 @@ Catch { Write-Output "No existing backup's to remove"}
 $summaryLog  = "${vmName}_summary.log"
 echo "Covers VSS Backup" > $summaryLog
 
-$remoteScript = "VSS_PartitionDisks.sh"
+$remoteScript = "STOR_VSS_PartitionDisks.sh"
 
 # Check input arguments
 if ($vmName -eq $null)
