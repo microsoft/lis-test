e8a8beb7c25d379140aa0cee951fa34330a697c6
Chris Valean <cvalean@cloudbasesolutions.com>
2015-05-20 00:13:32 +0300

    minor text changes

--- WS2008R2/lisa/remote-scripts/ica/CORE_reload_modules.sh
+++ WS2008R2/lisa/remote-scripts/ica/CORE_reload_modules.sh
@@ -5,11 +5,11 @@
 # Linux on Hyper-V and Azure Test Code, ver. 1.0.0
 # Copyright (c) Microsoft Corporation
 #
-# All rights reserved. 
+# All rights reserved.
 # Licensed under the Apache License, Version 2.0 (the ""License"");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#     http://www.apache.org/licenses/LICENSE-2.0  
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
 # OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
@@ -23,13 +23,13 @@
 
 ########################################################################
 #
-# CORE_StressReloadModules.sh
+# CORE_reload_modules.sh
 # Description:
 #    This script will first check the existence of Hyper-V kernel modules.
-#    Then it will reload the modules 500 times to stress the system.
+#    Then it will reload the LIS modules in a loop to stress the system.
 #    It also checks that hyperv_fb cannot be unloaded.
 #    When done it will bring up the eth0 interface and check again for 
-#    the presence of Hyper-V modules.
+#    the presence of the LIS modules.
 #     
 #    To pass test parameters into test cases, the host will create
 #    a file named constants.sh. This file contains one or more
@@ -102,9 +102,6 @@ VerifyModules()
     fi
     LogMsg "hv_netvsc loaded OK"
 
-    #
-    # Did utils load
-    #
     LogMsg "Checking if hv_utils loaded..."
 
     grep -q "utils" $MODULES
@@ -186,7 +183,6 @@ VerifyModules
  
 echo "Test ran for ${DIFF} seconds" >> ~/summary.log
 
-LogMsg "#########################################################"
 LogMsg "Result : Test Completed Successfully"
 LogMsg "Exiting with state: TestCompleted."
 UpdateTestState "TestCompleted"
