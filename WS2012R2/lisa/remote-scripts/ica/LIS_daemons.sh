97bef54f3b56102e26327f44dc214c144a0b17b4
Chris Valean <cvalean@cloudbasesolutions.com>
2016-01-14 19:21:08 +0200

    display the current running kernel version

--- WS2012R2/lisa/remote-scripts/ica/LIS_daemons.sh
+++ WS2012R2/lisa/remote-scripts/ica/LIS_daemons.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+
+########################################################################
+#
+# Linux on Hyper-V and Azure Test Code, ver. 1.0.0
+# Copyright (c) Microsoft Corporation
+#
+# All rights reserved. 
+# Licensed under the Apache License, Version 2.0 (the ""License"");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0  
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+# ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR
+# PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+#
+# See the Apache Version 2.0 License for specific language governing
+# permissions and limitations under the License.
+#
+########################################################################
 set -e
 set -x
 
@@ -7,6 +29,7 @@ ICA_TESTABORTED="TestAborted"       # Error during setup of test
 ICA_TESTFAILED="TestFailed"         # Error during execution of test
 
 CONSTANTS_FILE="constants.sh"
+LINUX_VERSION=$(uname -r)
 
 UpdateTestState()
 {
@@ -36,6 +59,12 @@ if [ -e ~/summary.log ]; then
     echo "Cleaning up previous copies of summary.log"
     rm -rf ~/summary.log
 fi
+
+#
+# Start the testing
+#
+UpdateSummary "KernelRelease=${LINUX_VERSION}"
+UpdateSummary "$(uname -a)"
 
 LinuxRelease()
 {
