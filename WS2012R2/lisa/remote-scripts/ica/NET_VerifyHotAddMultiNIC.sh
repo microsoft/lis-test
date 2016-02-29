8ce5ac7912fdd724487325306ff3bd1789500532
Adrian Suhov <asuhov@cloudbasesolutions.com>
2016-02-29 16:52:21 +0200

    Added NIC_HotAddRemove testcase
    1. Split up NET_HotAddMultiNIC in 2 testcases: First one checks the NIC
    Hot Add capability and second one checks for NIC Hot Remove capability.
    2. Added RHEL and Ubuntu support for both testcases

--- WS2012R2/lisa/remote-scripts/ica/NET_VerifyHotAddMultiNIC.sh
+++ WS2012R2/lisa/remote-scripts/ica/NET_VerifyHotAddMultiNIC.sh
@@ -23,6 +23,99 @@
 
 
 expectedCount=0
+declare os_VENDOR os_RELEASE os_UPDATE os_PACKAGE os_CODENAME
+
+########################################################################
+# Determine what OS is running
+########################################################################
+# GetOSVersion
+function GetOSVersion {
+    # Figure out which vendor we are
+    if [[ -x "`which sw_vers 2>/dev/null`" ]]; then
+        # OS/X
+        os_VENDOR=`sw_vers -productName`
+        os_RELEASE=`sw_vers -productVersion`
+        os_UPDATE=${os_RELEASE##*.}
+        os_RELEASE=${os_RELEASE%.*}
+        os_PACKAGE=""
+        if [[ "$os_RELEASE" =~ "10.7" ]]; then
+            os_CODENAME="lion"
+        elif [[ "$os_RELEASE" =~ "10.6" ]]; then
+            os_CODENAME="snow leopard"
+        elif [[ "$os_RELEASE" =~ "10.5" ]]; then
+            os_CODENAME="leopard"
+        elif [[ "$os_RELEASE" =~ "10.4" ]]; then
+            os_CODENAME="tiger"
+        elif [[ "$os_RELEASE" =~ "10.3" ]]; then
+            os_CODENAME="panther"
+        else
+            os_CODENAME=""
+        fi
+    elif [[ -x $(which lsb_release 2>/dev/null) ]]; then
+        os_VENDOR=$(lsb_release -i -s)
+        os_RELEASE=$(lsb_release -r -s)
+        os_UPDATE=""
+        os_PACKAGE="rpm"
+        if [[ "Debian,Ubuntu,LinuxMint" =~ $os_VENDOR ]]; then
+            os_PACKAGE="deb"
+        elif [[ "SUSE LINUX" =~ $os_VENDOR ]]; then
+            lsb_release -d -s | grep -q openSUSE
+            if [[ $? -eq 0 ]]; then
+                os_VENDOR="openSUSE"
+            fi
+        elif [[ $os_VENDOR == "openSUSE project" ]]; then
+            os_VENDOR="openSUSE"
+        elif [[ $os_VENDOR =~ Red.*Hat ]]; then
+            os_VENDOR="Red Hat"
+        fi
+        os_CODENAME=$(lsb_release -c -s)
+    elif [[ -r /etc/redhat-release ]]; then
+        # Red Hat Enterprise Linux Server release 5.5 (Tikanga)
+        # Red Hat Enterprise Linux Server release 7.0 Beta (Maipo)
+        # CentOS release 5.5 (Final)
+        # CentOS Linux release 6.0 (Final)
+        # Fedora release 16 (Verne)
+        # XenServer release 6.2.0-70446c (xenenterprise)
+        os_CODENAME=""
+        for r in "Red Hat" CentOS Fedora XenServer; do
+            os_VENDOR=$r
+            if [[ -n "`grep \"$r\" /etc/redhat-release`" ]]; then
+                ver=`sed -e 's/^.* \([0-9].*\) (\(.*\)).*$/\1\|\2/' /etc/redhat-release`
+                os_CODENAME=${ver#*|}
+                os_RELEASE=${ver%|*}
+                os_UPDATE=${os_RELEASE##*.}
+                os_RELEASE=${os_RELEASE%.*}
+                break
+            fi
+            os_VENDOR=""
+        done
+        os_PACKAGE="rpm"
+    elif [[ -r /etc/SuSE-release ]]; then
+        for r in openSUSE "SUSE Linux"; do
+            if [[ "$r" = "SUSE Linux" ]]; then
+                os_VENDOR="SUSE LINUX"
+            else
+                os_VENDOR=$r
+            fi
+
+            if [[ -n "`grep \"$r\" /etc/SuSE-release`" ]]; then
+                os_CODENAME=`grep "CODENAME = " /etc/SuSE-release | sed 's:.* = ::g'`
+                os_RELEASE=`grep "VERSION = " /etc/SuSE-release | sed 's:.* = ::g'`
+                os_UPDATE=`grep "PATCHLEVEL = " /etc/SuSE-release | sed 's:.* = ::g'`
+                break
+            fi
+            os_VENDOR=""
+        done
+        os_PACKAGE="rpm"
+    # If lsb_release is not installed, we should be able to detect Debian OS
+    elif [[ -f /etc/debian_version ]] && [[ $(cat /proc/version) =~ "Debian" ]]; then
+        os_VENDOR="Debian"
+        os_PACKAGE="deb"
+        os_CODENAME=$(awk '/VERSION=/' /etc/os-release | sed 's/VERSION=//' | sed -r 's/\"|\(|\)//g' | awk '{print $2}')
+        os_RELEASE=$(awk '/VERSION_ID=/' /etc/os-release | sed 's/VERSION_ID=//' | sed 's/\"//g')
+    fi
+    export os_VENDOR os_RELEASE os_UPDATE os_PACKAGE os_CODENAME
+}
 
 #
 # AddedNic ($ethCount)
@@ -40,8 +133,23 @@ function AddedNic
     #
     # Bring the new NIC online
     #
-    echo "Info : Creating ifcfg-eth1"
-    cp /etc/sysconfig/network/ifcfg-eth0 /etc/sysconfig/network/ifcfg-eth1
+    echo "os_VENDOR=$os_VENDOR"
+    if [[ "$os_VENDOR" == "Red Hat" ]] || \
+       [[ "$os_VENDOR" == "CentOS" ]]; then
+            echo "Info : Creating ifcfg-eth1"
+            cp /etc/sysconfig/network-scripts/ifcfg-eth0 /etc/sysconfig/network-scripts/ifcfg-eth1
+            sed -i -- 's/eth0/eth1/g' "/etc/sysconfig/network-scripts/ifcfg-eth1"
+    elif [ "$os_VENDOR" == "SUSE LINUX" ]; then
+            echo "Info : Creating ifcfg-eth1"
+            cp /etc/sysconfig/network/ifcfg-eth0 /etc/sysconfig/network/ifcfg-eth1
+            sed -i -- 's/eth0/eth1/g' /etc/sysconfig/network/ifcfg-eth1
+    elif [ "$os_VENDOR" == "Ubuntu" ]; then
+            echo "auto eth1" >> /etc/network/interfaces
+            echo "iface eth1 inet dhcp" >> /etc/network/interfaces
+    else
+        echo "Error: Linux Distro not supported!"
+        exit 1
+    fi
 
     echo "Info : Bringing up eth1"
     ifup eth1
@@ -50,13 +158,14 @@ function AddedNic
     # Verify the new NIC received an IP v4 address
     #
     echo "Info : Verify the new NIC has an IPv4 address"
-    ifconfig eth1 | grep -s "inet addr:" > /dev/null
+    ifconfig eth1 | grep -s "inet " > /dev/null
     if [ $? -ne 0 ]; then
         echo "Error: eth1 was not assigned an IPv4 address"
         exit 1
     fi
 
     echo "Info : eth1 is up"
+    echo "Info: NIC Hot Add test passed"
 }
 
 #
@@ -70,7 +179,32 @@ function RemovedNic
         exit 1
     fi
 
-    rm -f /etc/sysconfig/network/ifcfg-eth1
+    # Clean up files & check linux log for errors
+    if [[ "$os_VENDOR" == "Red Hat" ]] || \
+       [[ "$os_VENDOR" == "CentOS" ]]; then
+            echo "Info: Cleaning up RHEL/CentOS"
+            rm -f /etc/sysconfig/network-scripts/ifcfg-eth1
+            cat /var/log/messages | grep "unable to close device (ret -110)"
+            if [ $? -eq 0 ]; then
+                echo "Error: /var/log/messages reported netvsc throwed errors"
+            fi
+    elif [ "$os_VENDOR" == "SUSE LINUX" ]; then
+            rm -f /etc/sysconfig/network/ifcfg-eth1
+            cat /var/log/messages | grep "unable to close device (ret -110)"
+            if [ $? -eq 0 ]; then
+                echo "Error: /var/log/messages reported netvsc throwed errors"
+            fi
+    elif [ "$os_VENDOR" == "Ubuntu" ]; then
+            sed -i -e "/auto eth1/d" /etc/network/interfaces
+            sed -i -e "/iface eth1 inet dhcp/d" /etc/network/interfaces
+            cat /var/log/syslog | grep "unable to close device (ret -110)"
+            if [ $? -eq 0 ]; then
+                echo "Error: /var/log/syslog reported netvsc throwed errors"
+            fi
+    else
+        echo "Error: Linux Distro not supported!"
+        exit 1
+    fi
 }
 
 
@@ -96,6 +230,11 @@ ethCount=$(ifconfig -a | grep "^eth" | wc -l)
 echo "ethCount = ${ethCount}"
 
 #
+# Get data about Linux Distribution
+#
+GetOSVersion
+
+#
 # Set expectedCount based on the value of $1
 #
 case "$1" in
@@ -111,5 +250,4 @@ removed)
     ;;
 esac
 
-echo "Info : test passed"
 exit 0
