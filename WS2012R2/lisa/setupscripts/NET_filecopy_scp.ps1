8ec927496f337b8b633fa8622418182cbb99aa5d
paula crismaru <paulacrismaru@gmail.com>
2016-05-06 19:34:04 +0300

    net scp copy 2 files

--- WS2012R2/lisa/setupscripts/NET_filecopy_scp.ps1
+++ WS2012R2/lisa/setupscripts/NET_filecopy_scp.ps1
@@ -21,15 +21,15 @@
 
 <#
 .Synopsis
-    This script tests the functionality of copying a 10GB large file using pscp.
+    This script tests the functionality of copying 2 10GB large files using pscp.
 
 .Description
-    The script will copy a random generated 10GB file from a Windows host to
-	the Linux VM, and then checks if the size is matching.
+    The script will copy 2 random generated 10GB files from a Windows host to
+    the Linux VM, and then checks if the size is matching.
 
     A typical XML definition for this test case would look similar
     to the following:
-		<test>
+        <test>
             <testName>CopyFileScp</testName>
             <setupScript>setupScripts\Add-VHDXForResize.ps1</setupScript>
             <testScript>setupscripts\NET_filecopy_scp.ps1</testScript>
@@ -37,12 +37,11 @@
             <cleanupScript>SetupScripts\Remove-VHDXHardDisk.ps1</cleanupScript>
             <timeout>2500</timeout>
             <testParams>
-                <param>TC_COVERED=FCopy-04</param>
+                <param>TC_COVERED=NET-19</param>
+                <param>SCSI=1,0,Dynamic,512,15GB</param>
                     <param>Type=Fixed</param>
                     <param>SectorSize=512</param>
-                    <param>DefaultSize=15GB</param>
-                <param>SCSI=1,0,Dynamic,512,15GB</param>
-                <param>bandwidth=4096</param>
+                    <param>DefaultSize=20GB</param>
             </testParams>
         </test>
 
@@ -59,15 +58,18 @@
     setupScripts\NET_filecopy_scp.ps1 -vmName NameOfVm -hvServer localhost -testParams 'sshKey=path/to/ssh;ipv4=ipaddress'
 #>
 
+
 param([string] $vmName, [string] $hvServer, [string] $testParams)
 
 $testfile = $null
 # 10GB file size
-$filesize = 10737418240
+$filesize1 = 10737418240
+# 2GB file size
+$filesize2 = 10737418240
 
 #######################################################################
 #
-#	Checks if test file is present
+#   Checks if test file is present
 #
 #######################################################################
 function check_file([String] $testfile)
@@ -77,51 +79,99 @@ function check_file([String] $testfile)
         Write-Output "ERROR: Unable to read file /mnt/$testfile." -ErrorAction SilentlyContinue
         return $False
     }
-	return $True
+    return $True
 }
 
 #######################################################################
 #
-#	Mount disk
+#   Mount disk
 #
 #######################################################################
 function mount_disk()
 {
-	. .\setupScripts\TCUtils.ps1
+    . .\setupScripts\TCUtils.ps1
     $driveName = "/dev/sdb"
 
     $sts = SendCommandToVM $ipv4 $sshKey "(echo d;echo;echo w)|fdisk ${driveName}"
     if (-not $sts) {
-		Write-Output "ERROR: Failed to format the disk in the VM $vmName."
-		return $False
+        Write-Output "ERROR: Failed to format the disk in the VM $vmName."
+        return $False
     }
 
     $sts = SendCommandToVM $ipv4 $sshKey "(echo n;echo p;echo 1;echo;echo;echo w)|fdisk ${driveName}"
     if (-not $sts) {
-		Write-Output "ERROR: Failed to format the disk in the VM $vmName."
-		return $False
+        Write-Output "ERROR: Failed to format the disk in the VM $vmName."
+        return $False
     }
 
     $sts = SendCommandToVM $ipv4 $sshKey "mkfs.ext3 ${driveName}1"
     if (-not $sts) {
-		Write-Output "ERROR: Failed to make file system in the VM $vmName."
-		return $False
+        Write-Output "ERROR: Failed to make file system in the VM $vmName."
+        return $False
     }
 
     $sts = SendCommandToVM $ipv4 $sshKey "mount ${driveName}1 /mnt"
     if (-not $sts) {
-		Write-Output "ERROR: Failed to mount the disk in the VM $vmName."
-		return $False
+        Write-Output "ERROR: Failed to mount the disk in the VM $vmName."
+        return $False
     }
 
     "Info: $driveName has been mounted to /mnt in the VM $vmName."
 
     return $True
 }
+#####################
+#
+#   create file
+#
+#######################
+function create_file($filename, $filesize){
 
+    # Get VHD path of tested server; file will be copied there
+    $vhd_path = Get-VMHost -ComputerName $hvServer | Select -ExpandProperty VirtualHardDiskPath
+
+    # Fix path format if it's broken
+    if ($vhd_path.Substring($vhd_path.Length - 1, 1) -ne "\"){
+        $vhd_path = $vhd_path + "\"
+    }
+
+    $vhd_path_formatted = $vhd_path.Replace(':','$')
+
+    $filePath = $vhd_path + $filename
+    $file_path_formatted = $vhd_path_formatted + $filename
+
+    # Create a 10GB sample file
+    $createfile = fsutil file createnew \\$hvServer\$file_path_formatted $filesize
+
+    if ($createfile -notlike "File *testfile-* is created") {
+        "Error: Could not create the sample test file in the working directory! $file_path_formatted" | Tee-Object -Append -file $summaryLog
+        exit -1
+    }
+    return $filePath, $file_path_formatted
+}
+#############3
+#
+#   calculate local md5
+#
+###########
+function compute_local_md5($filePath){
+    #
+    #Getting MD5 checksum of the files
+    #
+    $localChksum = Get-FileHash $filePath -Algorithm MD5 | select -ExpandProperty hash
+    if (-not $?){
+        Write-Output "ERROR: Unable to get MD5 checksum"
+        Write-Output "ERROR: Unable to get MD5 checksum" >> $summaryLog
+        exit -1 
+    }
+    else {
+        Write-Output "MD5 checksum on Hyper-V: $localChksum" >> $summaryLog
+    }
+    return $localChksum
+}
 #######################################################################
 #
-#	Main body script
+#   Main body script
 
 #######################################################################
 
@@ -148,7 +198,6 @@ if (-not $testParams) {
 # Checking the mandatory testParams. New parameters must be validated here.
 #
 
-$bandwidth=2048
 $params = $testParams.Split(";")
 foreach ($p in $params) {
     $fields = $p.Split("=")
@@ -156,17 +205,14 @@ foreach ($p in $params) {
     if ($fields[0].Trim() -eq "TC_COVERED") {
         $TC_COVERED = $fields[1].Trim()
     }
-	if ($fields[0].Trim() -eq "rootDir") {
+    if ($fields[0].Trim() -eq "rootDir") {
         $rootDir = $fields[1].Trim()
     }
-	if ($fields[0].Trim() -eq "ipv4") {
-		$IPv4 = $fields[1].Trim()
+    if ($fields[0].Trim() -eq "ipv4") {
+        $IPv4 = $fields[1].Trim()
     }
-	if ($fields[0].Trim() -eq "sshkey") {
+    if ($fields[0].Trim() -eq "sshkey") {
         $sshkey = $fields[1].Trim()
-    }
-	if ($fields[0].Trim() -eq "bandwidth") {
-        $bandwidth = $fields[1].Trim()
     }
 }
 
@@ -192,138 +238,177 @@ $retVal = $True
 #
 if (-not (Test-Path -Path ".\bin\pscp.exe"))
 {
-	LogMsg 0 "Error: The putty utility .\bin\pscp.exe does not exist"
-	return
+    LogMsg 0 "Error: The putty utility .\bin\pscp.exe does not exist"
+    return
 }
 
-# Get VHD path of tested server; file will be copied there
-$vhd_path = Get-VMHost -ComputerName $hvServer | Select -ExpandProperty VirtualHardDiskPath
-
-# Fix path format if it's broken
-if ($vhd_path.Substring($vhd_path.Length - 1, 1) -ne "\"){
-    $vhd_path = $vhd_path + "\"
-}
-
-$vhd_path_formatted = $vhd_path.Replace(':','$')
 
 # Define the file-name to use with the current time-stamp
-$testfile = "testfile-$(get-date -uformat '%H-%M-%S-%Y-%m-%d').file"
+$testfile1 = "testfile-$(get-date -uformat '%H-%M-%S-%Y-%m-%d').file"
+$filePath1, $file_path_formatted1 = create_file $testfile1 $filesize1
 
-$filePath = $vhd_path + $testfile
-$file_path_formatted = $vhd_path_formatted + $testfile
+# second file
 
-# Create a 10GB sample file
-$createfile = fsutil file createnew \\$hvServer\$file_path_formatted $filesize
+# Define the file-name to use with the current time-stamp
+$testfile2 = "testfile-2-$(get-date -uformat '%H-%M-%S-%Y-%m-%d').file"
+$filePath2, $file_path_formatted2 = create_file $testfile2 $filesize2
 
-if ($createfile -notlike "File *testfile-*.file is created") {
-	"Error: Could not create the sample test file in the working directory!" | Tee-Object -Append -file $summaryLog
-	$retVal = $False
-}
-
+# mount disk
 $sts = mount_disk
 if (-not $sts[-1]) {
     Write-Output "ERROR: Failed to mount the disk in the VM." | Tee-Object -Append -file $summaryLog
     $retVal = $False
 }
 
-#
-#Getting MD5 checksum of the file
-#
-$localChksum = Get-FileHash $filePath -Algorithm MD5 | select -ExpandProperty hash
-if (-not $?){
-	Write-Output "ERROR: Unable to get MD5 checksum"
-	Write-Output "ERROR: Unable to get MD5 checksum" >> $summaryLog
-	RemoveTestFile
-	return $False   
-}
-else {
-	Write-Output "MD5 checksum on Hyper-V: $localChksum"
-}
+$localChksum1 = compute_local_md5 $filePath1
+$localChksum2 = compute_local_md5 $filePath2 
 
 #
 # Copy the file to the Linux guest VM
 #
 $Error.Clear()
+$command = "${rootDir}\bin\pscp -i ${rootDir}\ssh\${sshKey} ${filePath2} root@${ipv4}:/mnt/"
+$job = Start-Job -ScriptBlock  {Invoke-Expression $args[0]} -ArgumentList $command
+<#$job = Start-Job -ScriptBlock {$rootDir\bin\pscp -i $rootDir\ssh\${sshKey} ${filePath2} root@${ipv4}:/mnt/}#>
+$copyDuration1 = (Measure-Command { bin\pscp -i ssh\${sshKey} ${filePath1} root@${ipv4}:/mnt/ }).totalseconds
 
-$copyDuration = (Measure-Command { bin\pscp -i ssh\${sshKey} -l ${bandwidth} ${filePath} root@${ipv4}:/mnt/ }).totalseconds
+while ($True){
+    if ($job.state -eq "Completed"){
+            $copyDuration2 = ($job.PSEndTime - $job.PSBeginTime).seconds
+            Remove-Job -id $job.id
+       break
+    }
+}
+
 
 if ($Error.Count -eq 0) {
-	Write-Output "Info: File has been successfully copied to guest VM '${vmName}'" | Tee-Object -Append -file $summaryLog
+    Write-Output "Info: File has been successfully copied to guest VM '${vmName}'" | Tee-Object -Append -file $summaryLog
 }
 else {
-	Write-Output "ERROR: File could not be copied!" | Tee-Object -Append -file $summaryLog
-	$retVal = $False
+    Write-Output "ERROR: File could not be copied!" | Tee-Object -Append -file $summaryLog
+    $retVal = $False
 }
 
 [int]$copyDuration = [math]::floor($copyDuration)
 
-Write-Output "The file copy process took ${copyDuration} seconds with bandwidth $bandwidth" | Tee-Object -Append -file $summaryLog
+Write-Output "The file copy process took ${copyDuration1} and ${copyDuration2} seconds" | Tee-Object -Append -file $summaryLog
 
 #
 # Checking if the file is present on the guest and file size is matching
 #
-$sts = check_file $testfile
+$sts = check_file $testfile1
 if (-not $sts[-1]) {
-	Write-Output "ERROR: File is not present on the guest VM '${vmName}'!" | Tee-Object -Append -file $summaryLog
-	$retVal = $False
+    Write-Output "ERROR: File is not present on the guest VM '${vmName}'!" | Tee-Object -Append -file $summaryLog
+    $retVal = $False
 }
-elseif ($sts[0] -eq $filesize) {
-	Write-Output "Info: The file copied matches the 10GB size." | Tee-Object -Append -file $summaryLog
+elseif ($sts[0] -eq $filesize1) {
+    Write-Output "Info: The file copied matches the 10GB size." | Tee-Object -Append -file $summaryLog
 }
 else {
-	Write-Output "ERROR: The file copied doesn't match the 10GB size!" | Tee-Object -Append -file $summaryLog
-	$retVal = $False
+    Write-Output "ERROR: The file copied doesn't match the 10GB size!" | Tee-Object -Append -file $summaryLog
+    $retVal = $False
+}
+
+#
+# Checking if the file is present on the guest and file size is matching
+#
+$sts = check_file $testfile2
+if (-not $sts[-1]) {
+    Write-Output "ERROR: File is not present on the guest VM '${vmName}'!" | Tee-Object -Append -file $summaryLog
+    $retVal = $False
+}
+elseif ($sts[0] -eq $filesize2) {
+    Write-Output "Info: The file copied matches the 2GB size." | Tee-Object -Append -file $summaryLog
+}
+else {
+    Write-Output "ERROR: The file copied doesn't match the 2GB size!" | Tee-Object -Append -file $summaryLog
+    $retVal = $False
 }
 
 #
 # Run the remote script to get MD5 checksum on VM
 #
 
-.\bin\plink -i ssh\${sshKey} root@${ipv4} "dos2unix /root/NET_scp_check_md5.sh $testfile"
-.\bin\plink -i ssh\${sshKey} root@${ipv4} "bash /root/NET_scp_check_md5.sh $testfile"
+# first file
+$logfilename = ".\summary.log"
+.\bin\plink -i ssh\${sshKey} root@${ipv4} "dos2unix /root/FCOPY_check_md5.sh"
+
+.\bin\plink -i ssh\${sshKey} root@${ipv4} "bash /root/FCOPY_check_md5.sh $testfile1"
 if (-not $?) {
-    Write-Error -Message  "ERROR: Unable to compute md5 on vm file" -ErrorAction SilentlyContinue
-    Write-Output "ERROR: Unable to compute md5 on vm file"
+    Write-Error -Message  "ERROR: Unable to compute md5 on vm file 2" -ErrorAction SilentlyContinue
+    Write-Output "ERROR: Unable to compute md5 on vm file 2"
     return $False
 }
 
 .\bin\pscp -i ssh\${sshKey} root@${ipv4}:/root/summary.log .
 if (-not $?) {
-		Write-Error -Message "ERROR: Unable to copy the confirmation file from the VM" -ErrorAction SilentlyContinue
-		Write-Output "ERROR: Unable to copy the confirmation file from the VM"
-		return $False
-    }
-
-Write-Output "$remoteScript execution on VM: Success"
-Write-Output "Here are the remote logs:`n`n"
-$logfilename = ".\summary.log"
-Get-Content $logfilename
-Write-Output "$remoteScript execution on VM: Success" 
-
-#
-# Check if checksums are matching
-#
-$md5IsMatching = select-string -pattern $localChksum -path $logfilename
+    Write-Error -Message "ERROR: Unable to copy the confirmation file from the VM" -ErrorAction SilentlyContinue
+    Write-Output "ERROR: Unable to copy the confirmation file from the VM"
+    return $False
+}
+$md5IsMatching = select-string -pattern $localChksum1 -path $logfilename
 if ($md5IsMatching -eq $null) 
 { 
     Write-Output "ERROR: MD5 checksums are not matching" >> $summaryLog
-    Remove-Item -Path "NET_scp_check_md5.sh.log" -Force
+    Remove-Item -Path "FCOPY_check_md5.sh.log" -Force
     return $False
 } 
 else 
 { 
     Write-Output "Info: MD5 checksums are matching" | Tee-Object -Append -file $summaryLog
-    Remove-Item -Path "NET_scp_check_md5.sh.log" -Force
+    Remove-Item -Path "FCOPY_check_md5.sh.log" -Force
     $results = "Passed"
     $retVal = $True
 }
 
+<#Write-Output "$remoteScript execution on VM: Success"
+Write-Output "Here are the remote logs:`n`n"
+$logfilename = ".\summary.log"
+Get-Content $logfilename
+Write-Output "$remoteScript execution on VM: Success" 
+#>
+
+# 2nd file
+
+.\bin\plink -i ssh\${sshKey} root@${ipv4} "bash /root/FCOPY_check_md5.sh $testfile2"
+if (-not $?) {
+    Write-Error -Message  "ERROR: Unable to compute md5 on vm file 2" -ErrorAction SilentlyContinue
+    Write-Output "ERROR: Unable to compute md5 on vm file 2"
+    return $False
+}
+
+.\bin\pscp -i ssh\${sshKey} root@${ipv4}:/root/summary.log .
+if (-not $?) {
+    Write-Error -Message "ERROR: Unable to copy the confirmation file from the VM" -ErrorAction SilentlyContinue
+    Write-Output "ERROR: Unable to copy the confirmation file from the VM"
+    return $False
+}
+$md5IsMatching = select-string -pattern $localChksum2 -path $logfilename
+if ($md5IsMatching -eq $null) 
+{ 
+    Write-Output "ERROR: MD5 checksums are not matching" >> $summaryLog
+    Remove-Item -Path "FCOPY_check_md5.sh.log" -Force
+    return $False
+} 
+else 
+{ 
+    Write-Output "Info: MD5 checksums are matching" | Tee-Object -Append -file $summaryLog
+    Remove-Item -Path "FCOPY_check_md5.sh.log" -Force
+    $results = "Passed"
+    $retVal = $True
+}
+
+
 #
 # Removing the temporary test file
 #
-Remove-Item -Path \\$hvServer\$file_path_formatted -Force
+Remove-Item -Path \\$hvServer\$file_path_formatted1 -Force
 if (-not $?) {
-    Write-Output "ERROR: Cannot remove the test file '${testfile}'!" | Tee-Object -Append -file $summaryLog
+    Write-Output "ERROR: Cannot remove the test file '${testfile1}'!" | Tee-Object -Append -file $summaryLog
+}
+Remove-Item -Path \\$hvServer\$file_path_formatted2 -Force
+if (-not $?) {
+    Write-Output "ERROR: Cannot remove the test file '${testfile2}'!" | Tee-Object -Append -file $summaryLog
 }
 
 return $retVal
