1a9b78f55af5860679830dbae914ebc8c6c04b34
JingLi <jingli@redhat.com>
2016-06-07 17:57:23 +0800

    add Apache license and put case into xml
    Change-Id: I9eed94e52ffedbbca259e7c342d2273d057eeefc

--- WS2012R2/lisa/setupscripts/INST_timesync_pausedVM.ps1
+++ WS2012R2/lisa/setupscripts/INST_timesync_pausedVM.ps1
@@ -1,18 +1,38 @@
-#####################################################################
+########################################################################
 #
-# This test will check the time sync of guest OS with the host. It will pause the VM.
-# It will wait for 10 mins, resume the VM from paused state and will re-check the time sync.
+# Linux on Hyper-V and Azure Test Code, ver. 1.0.0
+# Copyright (c) Microsoft Corporation
 #
-# testParams
-#    HOME_DIR=C:\lisa\
-#    ipv4=
-#    sshKey=
+# All rights reserved. 
+# Licensed under the Apache License, Version 2.0 (the ""License"");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0  
 #
-# now=`date "+%m/%d/%Y %H:%M:%S%p"
-# returns 04/27/2012 16:10:30PM
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+# ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR
+# PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
 #
-#####################################################################
+# See the Apache Version 2.0 License for specific language governing
+# permissions and limitations under the License.
+##############################################################################
+<#
+.Synopsis
+    Verify the time sysc after VM paused
+.Description
+    This test will check the time sync of guest OS with the host. It will pause the VM.It will wait for 10 mins, 
+resume the VM from paused state and will re-check the time sync.
 
+ testParams
+    HOME_DIR=C:\lisa\
+    ipv4=
+    sshKey=
+
+ now=`date "+%m/%d/%Y %H:%M:%S%p"
+ returns 04/27/2012 16:10:30PM
+
+#>
 param ([String] $vmName, [String] $hvServer, [String] $testParams)
 
 #####################################################################
