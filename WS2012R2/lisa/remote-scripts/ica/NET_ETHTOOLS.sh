b98848c9aec0621cb19f52bda1b786a3c10998f7
Ovidiu Rusu <orusu@cloudbasesolutions.com>
2016-07-12 15:39:19 +0300

    Fixed ethtool script

--- WS2012R2/lisa/remote-scripts/ica/NET_ETHTOOLS.sh
+++ WS2012R2/lisa/remote-scripts/ica/NET_ETHTOOLS.sh
@@ -42,15 +42,19 @@
 #
 #############################################################################################################
 
+declare __iface_ignore
+declare __synth_iface
+declare -a ETHTOOL_KEYS
+ETHTOOL_KEYS=("tcp-segmentation-offload" "scatter-gather" "rx-checksumming" "tx-checksumming")
 
 # Convert eol
 dos2unix utils.sh
 
 # Source utils.sh
 . utils.sh || {
-	echo "Error: unable to source utils.sh!"
-	echo "TestAborted" > state.txt
-	exit 2
+    echo "Error: unable to source utils.sh!"
+    echo "TestAborted" > state.txt
+    exit 1
 }
 
 # Source constants file and initialize most common variables
@@ -58,70 +62,38 @@ UtilsInit
 
 # In case of error
 case $? in
-	0)
-		#do nothing, init succeeded
-		;;
-	1)
-		LogMsg "Unable to cd to $LIS_HOME. Aborting..."
-		UpdateSummary "Unable to cd to $LIS_HOME. Aborting..."
-		SetTestStateAborted
-		exit 3
-		;;
-	2)
-		LogMsg "Unable to use test state file. Aborting..."
-		UpdateSummary "Unable to use test state file. Aborting..."
-		# need to wait for test timeout to kick in
-			# hailmary try to update teststate
-			sleep 60
-			echo "TestAborted" > state.txt
-		exit 4
-		;;
-	3)
-		LogMsg "Error: unable to source constants file. Aborting..."
-		UpdateSummary "Error: unable to source constants file"
-		SetTestStateAborted
-		exit 5
-		;;
-	*)
-		# should not happen
-		LogMsg "UtilsInit returned an unknown error. Aborting..."
-		UpdateSummary "UtilsInit returned an unknown error. Aborting..."
-		SetTestStateAborted
-		exit 6
-		;;
+    0)
+        #do nothing, init succeeded
+        ;;
+    1)
+        LogMsg "Unable to cd to $LIS_HOME. Aborting..."
+        UpdateSummary "Unable to cd to $LIS_HOME. Aborting..."
+        SetTestStateAborted
+        exit 1
+        ;;
+    2)
+        LogMsg "Unable to use test state file. Aborting..."
+        UpdateSummary "Unable to use test state file. Aborting..."
+        # need to wait for test timeout to kick in
+        # hailmary try to update teststate
+        sleep 60
+        SetTestStateAborted
+        exit 1
+        ;;
+    3)
+        LogMsg "Error: unable to source constants file. Aborting..."
+        UpdateSummary "Error: unable to source constants file"
+        SetTestStateAborted
+        exit 1
+        ;;
+    *)
+        # should not happen
+        LogMsg "UtilsInit returned an unknown error. Aborting..."
+        UpdateSummary "UtilsInit returned an unknown error. Aborting..."
+        SetTestStateAborted
+        exit 1
+        ;;
 esac
-
-GetDistro
-
-case $DISTRO in
-    "redhat_7")
-       msg="Checking keys with ethtool ......"
-       LogMsg "${msg}"
-       UpdateSummary "${msg}"
-    ;;
-   "ubuntu_x")
-       msg="Failed: No ubuntu specific code to test case"
-       LogMsg "${msg}"
-       UpdateSummary "${msg}"
-       UpdateTestState $ICA_TESTABORTED
-       exit 1
-    ;;
-    "suse_x")
-       msg="Failed: No SUSE specific code to test case"
-       LogMsg "${msg}"
-       UpdateSummary "${msg}"
-       UpdateTestState $ICA_TESTABORTED
-       exit 1
-     ;;
-     *)
-       LogMsg "WARNING: Distro '${distro}' not supported."
-       UpdateSummary "WARNING: Distro '${distro}' not supported."
-       UpdateTestState $ICA_TESTABORTED
-       exit 1
-    ;;
-esac
-
-declare __iface_ignore
 
 # Parameter provided in constants file
 if [ "${ipv4:-UNDEFINED}" = "UNDEFINED" ]; then
@@ -129,17 +101,15 @@ if [ "${ipv4:-UNDEFINED}" = "UNDEFINED" ]; then
 	LogMsg "$msg"
 	UpdateSummary "$msg"
 	SetTestStateFailed
-    exit 10
+    exit 1
 else
-
 	CheckIP "$ipv4"
-
 	if [ 0 -ne $? ]; then
 		msg="Test parameter ipv4 = $ipv4 is not a valid IP Address"
 		LogMsg "$msg"
 		UpdateSummary "$msg"
 		SetTestStateFailed
-		exit 10
+		exit 1
 	fi
 
 	# Get the interface associated with the given ipv4
@@ -148,13 +118,12 @@ fi
 
 # Retrieve synthetic network interfaces
 GetSynthNetInterfaces
-
-declare -a ETHTOOL_KEYS
-ETHTOOL_KEYS=("tcp-segmentation-offload" "scatter-gather" "rx-checksumming" "tx-checksumming")
-
 if [ 0 -ne $? ]; then
 	msg="Warning, no synthetic network interfaces found"
 	LogMsg "$msg"
+    UpdateSummary "$msg"
+    SetTestStateFailed
+    exit 1
 else
 	# Remove interface if present
 	SYNTH_NET_INTERFACES=(${SYNTH_NET_INTERFACES[@]/$__iface_ignore/})
@@ -163,11 +132,11 @@ else
 		msg="The only synthetic interface is the one which LIS uses to send files/commands to the VM."
 		LogMsg "$msg"
 		UpdateSummary "$msg"
+        SetTestStateAborted
+        exit 1
 	fi
 
 	LogMsg "Found ${#SYNTH_NET_INTERFACES[@]} synthetic interface(s): ${SYNTH_NET_INTERFACES[*]} in VM"
-
-	declare __synth_iface
 
 	for __synth_iface in ${SYNTH_NET_INTERFACES[@]}; do
         ethtool -k $__synth_iface
@@ -176,19 +145,20 @@ else
             LogMsg "${msg}"
 			UpdateSummary "$msg"
 			SetTestStateFailed
-            exit 10
+            exit 1
         fi
 
         for __key in ${ETHTOOL_KEYS[@]}; do
             value=$(ethtool -k $__synth_iface |grep $__key |awk -F " " '{print $2}')
+            value=${value:0:2}
             if [ $value != "on" ]; then
-                msg="The ${__key} is not set as on"
+                msg="ERROR: The ${__key} is not set as on"
                 LogMsg "$msg"
                 UpdateSummary "$msg"
 			    SetTestStateFailed
-                exit 10
+                exit 1
             fi
-        done    
+        done
 		msg="The value of ${ETHTOOL_KEYS[@]} are set right"
 		LogMsg "$msg"
 		UpdateSummary "$msg"
