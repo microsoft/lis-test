2df9c44265a94ef945c8d691e98af3b63688c25f
Your Name <you@example.com>
2016-08-25 10:19:05 +0300

    Changing perf_utils.sh script location and adding Apache license

--- WS2012R2/lisa/remote-scripts/ica/perf_utils.sh
+++ WS2012R2/lisa/remote-scripts/ica/perf_utils.sh
@@ -1,4 +1,40 @@
 #!/usr/bin/env bash
+
+########################################################################
+#
+# Linux on Hyper-V and Azure Test Code, ver. 1.0.0
+# Copyright (c) Microsoft Corporation
+#
+# All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the ""License"");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+# ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR
+# PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+#
+# See the Apache Version 2.0 License for specific language governing
+# permissions and limitations under the License.
+#
+########################################################################
+
+########################################################################
+#
+# perf_utils.sh
+#
+# Description:
+#   Handle VM preparations for running performance tests
+#
+#   Steps:
+#   1. setup_sysctl - setting and applying sysctl parameters
+#   2. setup_io_scheduler - setting noop i/o scheduler on all disk type devices
+# (this is not a permanent change - on reboot it needs to be reapplied)
+#
+########################################################################
+
 declare -A sysctl_params=( ["net.core.netdev_max_backlog"]="30000"
                            ["net.core.rmem_max"]="67108864"
                            ["net.core.wmem_max"]="67108864"
