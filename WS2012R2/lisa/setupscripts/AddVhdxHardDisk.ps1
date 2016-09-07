d696a9bc9bdf7f5c982279f0560e30ff2682303d
Chris Valean <cvalean@cloudbasesolutions.com>
2016-09-07 14:23:08 +0300

    fix script error on Hyper-V 2012 - STOR

--- WS2012R2/lisa/setupscripts/AddVhdxHardDisk.ps1
+++ WS2012R2/lisa/setupscripts/AddVhdxHardDisk.ps1
@@ -110,7 +110,10 @@ param([string] $vmName, [string] $hvServer, [string] $testParams)
 
 $global:MinDiskSize = 1GB
 $global:DefaultDynamicSize = 127GB
-
+$SCSICount = 0
+$IDECount = 0
+$diskCount=$null
+$lun=$null
 
 #######################################################################
 #
@@ -326,14 +329,11 @@ function CreateHardDrive( [string] $vmName, [string] $server, [System.Boolean] $
     return $retVal
 }
 
-
-
 ############################################################################
 #
 # Main entry point for script
 #
 ############################################################################
-
 $retVal = $true
 
 #
@@ -358,14 +358,16 @@ if ($testParams -eq $null -or $testParams.Length -lt 3)
     return $False
 }
 
-#
-# Parse the testParams string
-#
-$SCSICount = 0
-$IDECount = 0
-$diskCount=$null
-$lun=$null
-$vmGeneration = Get-VM $vmName -ComputerName $hvServer | select -ExpandProperty Generation
+$OSInfo = get-wmiobject Win32_OperatingSystem -computerName $vm.hvServer
+if ( ($OSInfo.Caption -match '.2008 R2.') -or 
+     ($OSInfo.Caption -match '.2012$'))
+{
+   $vmGeneration = 1 
+}
+else
+{
+    $vmGeneration = Get-VM $vmName -ComputerName $hvServer| select -ExpandProperty Generation
+}
 
 $params = $testParams.TrimEnd(";").Split(";")
 foreach ($p in $params)
