41e07c27f8a534b801f99d52845f56aa6bbdc222
Bogdan Carpusor <bcarpusor@cloudbasesolutions.com>
2016-10-17 10:33:50 +0300

    Add test cases to check MAX NIC support
    Four new test cases are added to check if the VM can handle
    the maximum amount of NICs that Hyper-V can attach to it - 8 synthetic
    and 4 legacy, at the moment.
    
    The first 3 test cases, MaxSyntheticNIC, MaxLegacyNIC, MaxNIC,
    use the NET_ADD_MAX_NIC.ps1 setup script to add 7, 4 or 11 new
    NICs, depending on each case. They follow the same logic:
    	1. Check if VM sees the same amount of interfaces as the ones added
    	during setup
    	2. Configure each interface, using dhcp, bring each one up and
    	check if they have an IP address
    	3. Check external connection for each interface, after each
    	configuration and also after all interfaces are up
    	4. Compare the IP address values from KVP with the ones on the VM
    
    HotAddRemoveMaxNIC is targeted only to Gen 2 VMs. It uses the same logic
    of HotAddRemoveNIC, only with the maximum amount of NICs, that is
    permitted by Hyper-V.

---
+++ WS2012R2/lisa/remote-scripts/ica/NET_MAX_NIC.sh
@@ -0,0 +1,330 @@
+#!/bin/bash
+
+########################################################################
+#
+# Linux on Hyper-V and Azure Test Code, ver. 1.0.0
+# Copyright (c) Microsoft Corporation
+#
+# All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the ""License"");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+# ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR
+# PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+#
+# See the Apache Version 2.0 License for specific language governing
+# permissions and limitations under the License.
+#
+########################################################################
+
+function CheckGateway
+{
+	# Get interfaces that have default gateway set
+	gw_interf=($(route -n | grep 'UG[ \t]' | awk '{print $8}'))
+
+	[[ ${gw_interf[*]} =~ ${1} ]]
+}
+
+function AddGateway
+{
+	let max_attempts=3
+	let counter=1
+	let next_step=0
+	ifName=$1
+	while [ $next_step -eq 0 ];do
+		LogMsg "Info : Adding default gateway to interface ${ifName} on attempt ${counter}"
+		UpdateSummary "Info : Adding default gateway to interface ${ifName} on attempt ${counter}"
+		route add -net 0.0.0.0 gw ${DEFAULT_GATEWAY} netmask 0.0.0.0 dev ${ifName}
+
+		ip_status=$?
+		if [ $? -ne 0 ]; then
+			LogMsg "Error: Unable to add default gateway"
+			if [ $counter -eq $max_attempts ]; then
+				UpdateSummary "Error: Cannot add default gateway - ${DEFAULT_GATEWAY} for ${ifName} after ${max_attempts}"
+				SetTestStateFailed
+				return 0
+			else
+				let counter=$counter+1
+			fi
+		else
+			return 0
+		fi
+	done
+}
+
+function TestConnection
+{
+	if ping -q -c 3 -I ${2} -W 1 ${1} >/dev/null; then
+		msg="Info : Successfully pinged ${1} on interface ${2}"
+		LogMsg "${msg}"
+		UpdateSummary "${msg}"
+		SetTestStateRunning
+		return 0
+	else
+		msg="Info : Failed to ping ${1} on inteface ${2}"
+		LogMsg "${msg}"
+		UpdateSummary "${msg}"
+		return 1
+	fi
+}
+
+function ConfigureInterfaces
+{
+	for IFACE in ${IFACES[@]}; do
+		if [ $IFACE == "eth0" ]; then
+			continue
+		fi
+
+		# Get the specific nic name as seen by the VM
+		LogMsg "Info : Configuring interface ${IFACE}"
+		UpdateSummary "Info : Configuring interface ${IFACE}"
+		AddNIC $IFACE
+		if [ $? -eq 0 ]; then
+			ip_address=$(ip addr show $IFACE | grep "inet\b" | grep -v '127.0.0.1' | awk '{print $2}' | cut -d/ -f1)
+			msg="Info : Successfully set IP address - ${ip_address}"
+			LogMsg "${msg}"
+			UpdateSummary "${msg}"
+		else
+			return 1
+		fi
+
+		# Set filters for RHEL/CentOS
+		if [[ "$os_VENDOR" == "Red Hat" ]] || \
+		[[ "$os_VENDOR" == "CentOS" ]]; then
+			sysctl -w net.ipv4.conf.all.rp_filter=0
+			sysctl -w net.ipv4.conf.default.rp_filter=0
+			sysctl -w net.ipv4.conf.eth0.rp_filter=0
+			sysctl -w net.ipv4.conf.$IFACE.rp_filter=0
+			sleep 2
+		fi
+
+		# Chech for gateway
+		LogMsg "Info : Checking if default gateway is set for ${IFACE}"
+		CheckGateway $IFACE
+		if [ $? -ne 0 ];  then
+			LogMsg "Info : No gateway found for interface ${IFACE}"
+			UpdateSummary "Info : No gateway found for interface ${IFACE}"
+			route add -net 0.0.0.0 gw ${DEFAULT_GATEWAY} netmask 0.0.0.0 dev ${IFACE}
+			if [ $? -ne 0 ]; then
+				msg="Error : Unable to set default gateway - ${DEFAULT_GATEWAY}"
+				LogMsg "${msg}"
+				UpdateSummary "${msg}"
+				return 1
+			else
+				msg="Info: Default gateway - ${DEFAULT_GATEWAY} - was set for interface - ${IFACE}"
+				LogMsg "${msg}"
+				UpdateSummary "${msg}"
+			fi
+		fi
+
+		LogMsg "Checking external connection"
+		TestConnection $SUCCESS_PING $IFACE
+		if [ $? -ne 0 ]; then
+			return 1
+		fi
+	done
+
+	return 0
+}
+
+function AddNIC
+{
+	ifName=$1
+
+	#
+	# Bring the new NIC online
+	#
+	LogMsg "os_VENDOR=$os_VENDOR"
+	SetTestStateRunning
+	if [[ "$os_VENDOR" == "Red Hat" ]] || \
+	[[ "$os_VENDOR" == "CentOS" ]]; then
+		LogMsg "Info : Creating ifcfg-${ifName}"
+		cp /etc/sysconfig/network-scripts/ifcfg-eth0 /etc/sysconfig/network-scripts/ifcfg-${ifName}
+		sed -i -- "s/eth0/${ifName}/g" /etc/sysconfig/network-scripts/ifcfg-${ifName}
+		sed -i -e "s/HWADDR/#HWADDR/" /etc/sysconfig/network-scripts/ifcfg-${ifName}
+		sed -i -e "s/UUID/#UUID/" /etc/sysconfig/network-scripts/ifcfg-${ifName}
+	elif [ "$os_VENDOR" == "SUSE LINUX" ]; then
+		LogMsg "Info : Creating ifcfg-${ifName}"
+		cp /etc/sysconfig/network/ifcfg-eth0 /etc/sysconfig/network/ifcfg-${ifName}
+		sed -i -- "s/eth0/${ifName}/g" /etc/sysconfig/network/ifcfg-${ifName}
+		sed -i -e "s/HWADDR/#HWADDR/" /etc/sysconfig/network/ifcfg-${ifName}
+		sed -i -e "s/UUID/#UUID/" /etc/sysconfig/network/ifcfg-${ifName}
+	elif [ "$os_VENDOR" == "Ubuntu" ]; then
+		echo "auto ${ifName}" >> /etc/network/interfaces
+		echo "iface ${ifName} inet dhcp" >> /etc/network/interfaces
+	else
+		LogMsg "Error: Linux Distro not supported!"
+		UpdateSummary "Error: Linux Distro not supported!"
+		SetTestStateAborted
+		return 1
+	fi
+
+	# In some cases the interfaces does not receive an IP address from first try
+	let max_attempts=3
+	let counter=1
+	let next_step=0
+	while [ $next_step -eq 0 ];do
+		LogMsg "Info : Bringing up ${ifName} on attempt ${counter}"
+		UpdateSummary "Info : Bringing up ${ifName} on attempt ${counter}"
+		ifup ${ifName}
+
+		#
+		# Verify the new NIC received an IP v4 address
+		#
+		LogMsg "Info : Verify the new NIC has an IPv4 address}"
+		ifconfig ${ifName} | grep -s "inet " > /dev/null
+		ip_status=$?
+		if [ $? -ne 0 ]; then
+			LogMsg "Error: ${ifName} was not assigned an IPv4 address"
+			if [ $counter -eq $max_attempts ]; then
+				UpdateSummary "Error: ${ifName} was not assigned an IPv4 address"
+				SetTestStateFailed
+				return 1
+			else
+				let counter=$counter+1
+			fi
+		else
+			let next_step=1
+		fi
+	done
+
+	LogMsg "Info : ${ifName} is up"
+	return 0
+}
+
+#######################################################################
+#
+# Main script body
+#
+#######################################################################
+#
+# Get data about Linux Distribution
+#
+# Convert eol
+dos2unix utils.sh
+
+# Source utils.sh
+. utils.sh || {
+	echo "Error: unable to source utils.sh!"
+	exit 2
+}
+
+UtilsInit
+
+if [ "${TEST_TYPE:-UNDEFINED}" = "UNDEFINED" ]; then
+	msg="Error : Parameter TEST_TYPE was not found"
+	LogMsg "${msg}"
+	UpdateSummary "${msg}"
+	SetTestStateAborted
+	exit 30
+else
+	IFS=',' read -a TYPE <<< "$TEST_TYPE"
+fi
+
+if [ "${SYNTHETIC_NICS:-UNDEFINED}" = "UNDEFINED" ] && [ "${LEGACY_NICS:-UNDEFINED}" = "UNDEFINED" ]; then
+	msg="Error : Parameters SYNTHETIC_NICS or LEGACY_NICS were not found"
+	LogMsg "${msg}"
+	UpdateSummary "${msg}"
+	SetTestStateAborted
+	exit 30
+fi
+
+let EXPECTED_INTERFACES_NO=1
+if [ -z "${SYNTHETIC_NICS+x}" ]; then
+	LogMsg "Parameter SYNTHETIC_NICS was not found"
+else
+	let EXPECTED_INTERFACES_NO=$EXPECTED_INTERFACES_NO+$SYNTHETIC_NICS
+fi
+
+if [ -z "${LEGACY_NICS+x}" ]; then
+	LogMsg "Parameter SYNTHETIC_NICS was not found"
+else
+	let EXPECTED_INTERFACES_NO=$EXPECTED_INTERFACES_NO+$LEGACY_NICS
+fi
+
+if [ "${FOUND_PARAM}" -eq 0 ]; then
+	msg="Error : Parameter TEST_TYPE was not found"
+	LogMsg "${msg}"
+	UpdateSummary "${msg}"
+	SetTestStateAborted
+	exit 30
+fi
+GetOSVersion
+DEFAULT_GATEWAY=($(route -n | grep 'UG[ \t]' | awk '{print $2}'))
+SUCCESS_PING="8.8.8.8"
+
+IFACES=($(ifconfig -s -a | awk '{print $1}'))
+# Delete first and last elements from the list- iface, lo
+IFACES=("${IFACES[@]:1}")
+unset IFACES[${#IFACES[@]}-1]
+# Check for interfaces with longer names - enp0s10f
+let COUNTER=0
+for i in "${!IFACES[@]}"; do
+	if [ ${IFACES[$i]} == "enp0s10f" ]; then
+		IFACES[$i]=${IFACES[$i]}${COUNTER}
+		let COUNTER=COUNTER+1
+	fi
+done
+
+#
+# Check how many interfaces are visible to the VM
+#
+if [ ${#IFACES[@]} -ne ${EXPECTED_INTERFACES_NO} ]; then
+	msg="Error : Test expected ${EXPECTED_INTERFACES_NO} interfaces to be visible on VM. Found ${#IFACES[@]} interfaces"
+	LogMsg "${msg}"
+	UpdateSummary "${msg}"
+	SetTestStateFailed
+	exit 30
+fi
+
+#
+# Bring interfaces up, using dhcp, and check connection for each one
+#
+ConfigureInterfaces
+if [ $? -ne 0 ]; then
+	SetTestStateFailed
+	exit 1
+fi
+#
+# Check if all interfaces have a default gateway
+#
+GATEWAY_IF=($(route -n | grep 'UG[ \t]' | awk '{print $8}'))
+if [ ${#GATEWAY_IF[@]} -ne $EXPECTED_INTERFACES_NO ]; then
+	LogMsg "Info : Checking interfaces with missing gateway address"
+	UpdateSummary "Info : Checking interfaces with missing gateway address"
+	for IFACE in ${IFACES[@]}; do
+		CheckGateway $IFACE
+		if [ $? -ne 0 ]; then
+			LogMsg "WARNING : No gateway found for interface ${IFACE}"
+			route add -net 0.0.0.0 gw ${DEFAULT_GATEWAY} netmask 0.0.0.0 dev ${IFACE}
+			if [ $? -ne 0 ]; then
+				msg="Error : Unable to set default gateway - ${DEFAULT_GATEWAY}"
+				LogMsg "${msg}"
+				UpdateSummary "${msg}"
+				SetTestStateFailed
+				exit 2
+			fi
+		fi
+		
+	done
+fi
+
+#
+# Check to see if all raised interfaces work
+#
+for IFACE in ${IFACES[@]}; do
+	TestConnection $SUCCESS_PING $IFACE
+	if [ $? -ne 0 ]; then
+		SetTestStateFailed
+		exit 1
+	fi
+done
+
+LogMsg "Test run completed"
+UpdateSummary "Test run completed"
+SetTestStateCompleted
+exit 0
