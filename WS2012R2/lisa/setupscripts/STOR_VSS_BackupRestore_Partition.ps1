6ceec1a3abf338514fda47f0783eef69c419602b
Chris Valean <cvalean@cloudbasesolutions.com>
2017-02-27 21:56:09 +0200

    VSS tests code cleanup
    Consolidating functions and overall code cleanup.
    
    VSS tests code cleanup
    
    VSS tests code cleanup

--- WS2012R2/lisa/setupscripts/STOR_VSS_BackupRestore_Partition.ps1
+++ WS2012R2/lisa/setupscripts/STOR_VSS_BackupRestore_Partition.ps1
@@ -54,7 +54,7 @@
     Name of the VM to remove disk from .
 
 .Parameter hvServer
-    Name of the Hyper-V server hosting the VM.
+    Name of the VM to backup/restore.
 
 .Parameter testParams
     Test data for this test case
@@ -66,60 +66,14 @@
 
 param([string] $vmName, [string] $hvServer, [string] $testParams)
 
-#######################################################################
-# Check boot.msg in Linux VM for Recovering journal. 
-#######################################################################
-function CheckRecoveringJ()
-{
-    $retValue = $False
-       
-    .\bin\pscp -i ssh\${sshKey}  root@${ipv4}:/var/log/boot.* ./boot.msg 
-
-    if (-not $?)
-    {
-      Write-Output "ERROR: Unable to copy boot.msg from the VM"
-       return $False
-    }
-
-    $filename = ".\boot.msg"
-    $text = "recovering journal"
-    
-    $file = Get-Content $filename
-    if (-not $file)
-    {
-        Write-Error -Message "Unable to read file" -Category InvalidArgument -ErrorAction SilentlyContinue
-        return $null
-    }
-
-     foreach ($line in $file)
-    {
-        if ($line -match $text)
-        {           
-            $retValue = $True 
-            Write-Output "$line"          
-        }             
-    }
-
-    del $filename
-    return $retValue    
-}
+$retVal = $false
+$remoteScript = "PartitionDisks.sh"
 
 ####################################################################### 
 # 
 # Main script body 
 # 
 #######################################################################
-$retVal = $false
-
-Write-Output "Removing old backups"
-try { Remove-WBBackupSet -Force -WarningAction SilentlyContinue }
-Catch { Write-Output "No existing backup's to remove"}
-
-# Define and cleanup the summaryLog
-$summaryLog  = "${vmName}_summary.log"
-echo "Covers VSS Backup" > $summaryLog
-
-$remoteScript = "PartitionDisks.sh"
 
 # Check input arguments
 if ($vmName -eq $null)
@@ -136,6 +90,7 @@ foreach ($p in $params)
     $fields = $p.Split("=")
         switch ($fields[0].Trim())
         {
+		"TC_COVERED" { $TC_COVERED = $fields[1].Trim() }
         "sshKey" { $sshKey = $fields[1].Trim() }
         "ipv4" { $ipv4 = $fields[1].Trim() }
         "rootdir" { $rootDir = $fields[1].Trim() }
@@ -175,13 +130,29 @@ if ($null -eq $FILESYS)
     return $False
 }
 
-echo $params
-
 # Change the working directory to where we need to be
 cd $rootDir
 
-# Source the TCUtils.ps1 file
-. .\setupscripts\TCUtils.ps1
+#
+# Delete any summary.log from a previous test run, then create a new file
+#
+$summaryLog = "${vmName}_summary.log"
+del $summaryLog -ErrorAction SilentlyContinue
+Write-output "This script covers test case: ${TC_COVERED}" | Tee-Object -Append -file $summaryLog
+
+# Source TCUtils.ps1 for common functions
+if (Test-Path ".\setupScripts\TCUtils.ps1") {
+	. .\setupScripts\TCUtils.ps1
+	"Info: Sourced TCUtils.ps1"
+}
+else {
+	"Error: Could not find setupScripts\TCUtils.ps1"
+	return $false
+}
+
+Write-Output "Info: Removing old backups"
+try { Remove-WBBackupSet -Force -WarningAction SilentlyContinue }
+Catch { Write-Output "No existing backup's to remove"}
 
 # Check if the Vm VHD in not on the same drive as the backup destination 
 $vm = Get-VM -Name $vmName -ComputerName $hvServer
@@ -201,14 +172,6 @@ foreach ($drive in $vm.HardDrives)
     }
 }
 
-# Send utils.sh to VM
-echo y | .\bin\pscp -i ssh\${sshKey} .\remote-scripts\ica\utils.sh root@${ipv4}:
-if (-not $?)
-{
-    Write-Output "ERROR: Unable to copy utils.sh to the VM"
-    return $False
-} 
-
 # Check to see Linux VM is running VSS backup daemon 
 $sts = RunRemoteScript "STOR_VSS_Check_VSS_Daemon.sh"
 if (-not $sts[-1])
@@ -218,12 +181,7 @@ if (-not $sts[-1])
     return $False
 }
 
-Write-Output "VSS Daemon is running " >> $summaryLog
-
-# Install the Windows Backup feature
-Write-Output "Checking if the Windows Server Backup feature is installed..."
-try { Add-WindowsFeature -Name Windows-Server-Backup -IncludeAllSubFeature:$true -Restart:$false }
-Catch { Write-Output "Windows Server Backup feature is already installed, no actions required."}
+Write-Output "Info: VSS Daemon is running" >> $summaryLog
 
 # Run the remote script
 $sts = RunRemoteScript $remoteScript
@@ -269,9 +227,6 @@ $VM = Get-WBVirtualMachine | where vmname -like $vmName
 Add-WBVirtualMachine -Policy $policy -VirtualMachine $VM
 Add-WBBackupTarget -Policy $policy -Target $backupLocation
 
-# Display the Backup policy
-Write-Output "Backup policy is: `n$policy"
-
 # Start the backup
 Write-Output "Backing to $driveletter"
 Start-WBBackup -Policy $policy
@@ -290,12 +245,12 @@ if ($sts.JobState -ne "Completed" -or $sts.HResult -ne 0)
     return $retVal
 }
 
-Write-Output "`nBackup success!`n"
+Write-Output "`nInfo: Backup successful!`n"
 # Let's wait a few Seconds
 Start-Sleep -Seconds 70
 
 # Start the Restore
-Write-Output "`nNow let's do restore ...`n"
+Write-Output "`nNow let's restore the VM from backup...`n"
 
 # Get BackupSet
 $BackupSet=Get-WBBackupSet -BackupTarget $backupLocation
