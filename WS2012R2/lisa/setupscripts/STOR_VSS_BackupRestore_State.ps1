95df5ee59f24f1c89f1768180d5dc0109cd47beb
Bogdan Carpusor <bcarpusor@cloudbasesolutions.com>
2017-03-09 11:55:30 +0200

    Refactored VSS powershell scripts
    Common code blocks from VSS test scripts were
    moved to separate, single purpose functions in
    the STOR_VSS_Utils.ps1 script.

--- WS2012R2/lisa/setupscripts/STOR_VSS_BackupRestore_State.ps1
+++ WS2012R2/lisa/setupscripts/STOR_VSS_BackupRestore_State.ps1
@@ -87,7 +87,7 @@ function ChangeVMState($vmState,$vmName)
     }
     elseif ($vmState -eq "Saved")
     {
-        Save-VM -Name $vmName -ErrorAction SilentlyContinue
+        Save-VM -Name $vmName -Action SilentlyContinue
         return $vm.state
     }
     elseif ($vmState -eq "Paused") 
@@ -183,37 +183,24 @@ else {
 	return $false
 }
 
-Write-Output "Info: Removing old backups"
-try { Remove-WBBackupSet -Force -WarningAction SilentlyContinue }
-Catch { Write-Output "No existing backup's to remove"}
 
-# Check if the VM VHD in not on the same drive as the backup destination 
-$vm = Get-VM -Name $vmName -ComputerName $hvServer
-if (-not $vm)
-{
-    "Error: VM '${vmName}' does not exist"
-    return $False
+# Source STOR_VSS_Utils.ps1 for common VSS functions
+if (Test-Path ".\setupScripts\STOR_VSS_Utils.ps1") {
+	. .\setupScripts\STOR_VSS_Utils.ps1
+	"Info: Sourced STOR_VSS_Utils.ps1"
 }
- 
-foreach ($drive in $vm.HardDrives)
-{
-    if ( $drive.Path.StartsWith("${driveLetter}"))
-    {
-        "Error: Backup partition '${driveLetter}' is same as partition hosting the VMs disk"
-        "       $($drive.Path)"
-        return $False
-    }
+else {
+	"Error: Could not find setupScripts\STOR_VSS_Utils.ps1"
+	return $false
 }
 
-# Check to see Linux VM is running VSS backup daemon 
-$sts = CheckVSSDaemon
-if (-not $sts[-1])
+
+$sts = runSetup $vmName $hvServer $driveletter
+if (-not $sts[-1]) 
 {
-    Write-Output "ERROR: VSS backup daemon is not running inside Linux VM"
     return $False
 }
 
-Write-Output "Info: VSS Daemon is running" >> $summaryLog
 
 # Check if VM is Started
 $vm = Get-VM -Name $vmName
@@ -241,115 +228,36 @@ elseif ( $sts -ne $vmState )
 
 Write-Output "State change of $vmName to $vmState : Success."
 
-# Remove Existing Backup Policy
-try { Remove-WBPolicy -all -force }
-Catch { Write-Output "No existing backup policy to remove"}
 
-Write-Output "Removing old backups"
-try { Remove-WBBackupSet -Force -WarningAction SilentlyContinue }
-Catch { Write-Output "No existing backup's to remove"}
-
-# Set up a new Backup Policy
-$policy = New-WBPolicy
-
-# Set the backup backup location
-$backupLocation = New-WBBackupTarget -VolumePath $driveletter
-
-# Define VSS WBBackup type
-Set-WBVssBackupOptions -Policy $policy -VssCopyBackup
-
-# Add the Virtual machines to the list
-$VM = Get-WBVirtualMachine | where vmname -like $vmName
-Add-WBVirtualMachine -Policy $policy -VirtualMachine $VM
-Add-WBBackupTarget -Policy $policy -Target $backupLocation
-
-# Start the backup
-Write-Output "Backing to $driveletter"
-Start-WBBackup -Policy $policy
-
-# Review the results            
-$BackupTime = (New-Timespan -Start (Get-WBJob -Previous 1).StartTime -End (Get-WBJob -Previous 1).EndTime).Minutes
-Write-Output "Backup duration: $BackupTime minutes"           
-"Backup duration: $BackupTime minutes" >> $summaryLog
-
-$sts=Get-WBJob -Previous 1
-if ($sts.JobState -ne "Completed" -or $sts.HResult -ne 0)
+$sts = startBackup $vmName $driveLetter
+if (-not $sts[-1])
 {
-    Write-Output "ERROR: VSS Backup failed"
-    Write-Output $sts.ErrorDescription
-    $retVal = $false
-    return $retVal
-}
-
-Write-Output "`nInfo: Backup successful!`n"
-# Let's wait a few Seconds
-Start-Sleep -Seconds 70
-
-# Start the Restore
-Write-Output "`nNow let's restore the VM from backup...`n"
-
-# Get BackupSet
-$BackupSet=Get-WBBackupSet -BackupTarget $backupLocation
-
-# Start Restore
-Start-WBHyperVRecovery -BackupSet $BackupSet -VMInBackup $BackupSet.Application[0].Component[0] -Force -WarningAction SilentlyContinue
-$sts=Get-WBJob -Previous 1
-if ($sts.JobState -ne "Completed" -or $sts.HResult -ne 0)
-{
-    Write-Output "ERROR: VSS Restore failed"
-    Write-Output $sts.ErrorDescription
-    $retVal = $false
-    return $retVal
-}
-
-# Review the results  
-$RestoreTime = (New-Timespan -Start (Get-WBJob -Previous 1).StartTime -End (Get-WBJob -Previous 1).EndTime).Minutes
-Write-Output "Restore duration: $RestoreTime minutes"
-"Restore duration: $RestoreTime minutes" >> $summaryLog
-
-# Make sure VM exsist after VSS backup/restore Operation 
-$vm = Get-VM -Name $vmName -ComputerName $hvServer
-    if (-not $vm)
-    {
-        Write-Output "ERROR: VM ${vmName} does not exist after restore"
-        return $False
-    }
-Write-Output "Restore success!"
-
-# Now Start the VM 
-$timeout = 300
-$sts = Start-VM -Name $vmName -ComputerName $hvServer 
-if (-not (WaitForVMToStartKVP $vmName $hvServer $timeout ))
-{
-    Write-Output "ERROR: ${vmName} failed to start"
-    return $False
-}
-else
-{
-    Write-Output "INFO: Started VM ${vmName}"
-}
-
-start-sleep -s 120
-# Now Check the boot logs in VM to verify if there is no Recovering journals in it . 
-$sts=CheckRecoveringJ
-if ($sts[-1])
-{
-    Write-Output "ERROR: Recovering Journals in Boot log File, VSS Backup/restore is Failed "
-    Write-Output "No Recovering Journal in boot logs: Failed" >> $summaryLog
     return $False
 }
 else 
 {
-    $results = "Passed"
-    $retVal = $True
-    Write-Output "INFO: VSS Back/Restore: Success"   
-    Write-Output "Recovering Journal in boot msg: Success" >> $summaryLog
+    $backupLocation = $sts
 }
 
-# Remove Created Backup
-Write-Output "Removing old backups from $backupLocation"
-try { Remove-WBBackupSet -BackupTarget $backupLocation -Force -WarningAction SilentlyContinue }
-Catch { Write-Output "No existing backup's to remove"}
+$sts = restoreBackup $backupLocation
+if (-not $sts[-1])
+{   
+    return $False
+}
+
+$sts = checkResults $vmName $hvServer
+if (-not $sts[-1]) 
+{   
+    $retVal = $False
+} 
+else 
+{
+	$retVal = $True
+    $results = $sts
+}
+
+
+runCleanup $backupLocation
 
 Write-Output "INFO: Test ${results}"
 return $retVal
