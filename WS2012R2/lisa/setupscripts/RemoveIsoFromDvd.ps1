2d6ad9b9c8bc717116d301b91495ee8862c1843c
Chris Valean <cvalean@cloudbasesolutions.com>
2017-03-23 07:49:48 +0200

    Code cleanup

--- WS2012R2/lisa/setupscripts/RemoveIsoFromDvd.ps1
+++ WS2012R2/lisa/setupscripts/RemoveIsoFromDvd.ps1
@@ -3,11 +3,11 @@
 # Linux on Hyper-V and Azure Test Code, ver. 1.0.0
 # Copyright (c) Microsoft Corporation
 #
-# All rights reserved. 
+# All rights reserved.
 # Licensed under the Apache License, Version 2.0 (the ""License"");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#     http://www.apache.org/licenses/LICENSE-2.0  
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
 # OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
@@ -19,6 +19,14 @@
 #
 ########################################################################
 
+#######################################################################
+#
+# RemoveIsoFromDvd.ps1
+#
+# Description:
+#    This script will "unmount" a .iso file in the DVD drive
+#
+#######################################################################
 
 <#
 .Synopsis
@@ -28,7 +36,7 @@
     Remove the .iso file from the default DVD drive.
 
 .Parameter vmName
-    Name of the VM that will a DVD drive modified.
+    Name of the test VM.
 
 .Parameter hvServer
     Name of the Hyper-V server hosting the VM.
@@ -40,27 +48,7 @@
     .\RemoveIsoFromDvd.ps1 "MyVm" "localhost"
 #>
 
-#######################################################################
-#
-# RemoveIsoFromDvd.ps1
-#
-# Description:
-#    This script will "unmount" a .iso file in the DVD drive (IDE 1 0)
-#
-#######################################################################
-
 param ([String] $vmName, [String] $hvServer, [String] $testParams)
-
-
-#######################################################################
-#
-# Main script body
-#
-#######################################################################
-"removeIsoFromDvd.ps1"
-"  vmName = ${vmName}"
-"  hvServer = ${hvServer}"
-"  testParams = ${testParams}"
 
 $isoFilename = $null
 $controllerNumber=$null
@@ -86,10 +74,11 @@ if (-not $hvServer)
 #
 $error.Clear()
 
+#######################################################################
 #
-# Remove the .iso file from the VMs DVD drive
+# Main script body
 #
-
+#######################################################################
 $vmGeneration = Get-VM $vmName -ComputerName $hvServer| select -ExpandProperty Generation -ErrorAction SilentlyContinue
 if ($? -eq $False)
 {
