87cb33453b61a83d6231d1071ad7cd0828c4b801
boyang <758298111@qq.com>
2017-07-11 13:16:42 +0800

    Fix summary.log from vm always is covered by new case

--- WS2012R2/lisa/setupscripts/Kdump.ps1
+++ WS2012R2/lisa/setupscripts/Kdump.ps1
@@ -21,14 +21,6 @@
 
 param([string] $vmName, [string] $hvServer, [string] $testParams)
 
-$retVal = $false
-$TC_COVERED = $null
-$sshKey = $null
-$ipv4 = $null
-$nmi = $null
-# variable to define if a NFS location should be used for the crash files
-$use_nfs = $null
-
 #
 # Check input arguments
 #
@@ -42,58 +34,88 @@ if ($hvServer -eq $null) {
     return $retVal
 }
 
-$params = $testParams.Split(";")
+if ($testParams -eq $null)
+{
+    Throw "Error: No test parameters specified"
+}
 
+#
+# Parse test parameters
+#
+$retVal = $false
+$TC_COVERED = $null
+$sshKey = $null
+$ipv4 = $null
+$nmi = $null
+# variable to define if a NFS location should be used for the crash files
+$use_nfs = $null
+
+$params = $testParams.Split(";")
 foreach ($p in $params) {
     $fields = $p.Split("=")
 
     switch ($fields[0].Trim()) {
-        "rootDir"   { $rootDir = $fields[1].Trim() }
-	"TC_COVERED" { $TC_COVERED = $fields[1].Trim() }
-        "sshKey" { $sshKey  = $fields[1].Trim() }
-        "ipv4"   { $ipv4    = $fields[1].Trim() }
-        "crashkernel"   { $crashkernel    = $fields[1].Trim() }
-        "TestLogDir" { $logdir = $fields[1].Trim() }
-        "NMI" { $nmi = $fields[1].Trim() }
-        "VM2NAME" { $vm2Name = $fields[1].Trim() }
-		"use_nfs" { $use_nfs = $fields[1].Trim() }
-        default  {}
+      "rootDir"       { $rootDir = $fields[1].Trim() }
+      "TC_COVERED"    { $TC_COVERED = $fields[1].Trim() }
+      "sshKey"        { $sshKey  = $fields[1].Trim() }
+      "ipv4"          { $ipv4    = $fields[1].Trim() }
+      "crashkernel"   { $crashkernel    = $fields[1].Trim() }
+      "TestLogDir"    { $logdir = $fields[1].Trim() }
+      "NMI"           { $nmi = $fields[1].Trim() }
+      "VM2NAME"       { $vm2Name = $fields[1].Trim() }
+      "use_nfs"       { $use_nfs = $fields[1].Trim() }
+      default         {}
     }
 }
 
 if ($null -eq $sshKey) {
     "Error: Test parameter sshKey was not specified"
-    return $False
+    return $false
 }
 
 if ($null -eq $ipv4) {
     "Error: Test parameter ipv4 was not specified"
-    return $False
+    return $false
 }
 
+#
 # Change the working directory to where we need to be
+#
 if (-not (Test-Path $rootDir)) {
     "Error: The directory `"${rootDir}`" does not exist!"
-    return $False
+    return $false
 }
 cd $rootDir
 
+if ($null -eq $crashkernel)
+{
+	"FAIL: Test parameter crashkernel was not specified"
+	return $false
+}
+
+#
 # Delete any previous summary.log file
+#
 $summaryLog = "${vmName}_summary.log"
 del $summaryLog -ErrorAction SilentlyContinue
 Write-Output "This script covers test case: ${TC_COVERED}" | Tee-Object -Append -file $summaryLog
 
+#
 # Source TCUtils.ps1 for test related functions
-  if (Test-Path ".\setupScripts\TCUtils.ps1")
-  {
-    . .\setupScripts\TCUtils.ps1
-  }
-  else
-  {
-    "Error: Could not find setupScripts\TCUtils.ps1"
-    return $false
-  }
+#
+if (Test-Path ".\setupScripts\TCUtils.ps1")
+{
+  . .\setupScripts\TCUtils.ps1
+}
+else
+{
+  "Error: Could not find setupScripts\TCUtils.ps1"
+  return $false
+}
 
+#
+# Confirm the second VM and NFS
+#
 if ($vm2Name -And $use_nfs -eq "yes")
 {
     $checkState = Get-VM -Name $vm2Name -ComputerName $hvServer
@@ -105,7 +127,7 @@ if ($vm2Name -And $use_nfs -eq "yes")
         {
             "Error: Unable to start VM ${vm2Name}"
             $error[0].Exception
-            return $False
+            return $false
         }
         $timeout = 240 # seconds
         if (-not (WaitForVMToStartKVP $vm2Name $hvServer $timeout))
@@ -122,15 +144,17 @@ if ($vm2Name -And $use_nfs -eq "yes")
         {
             "Error: VM ${vm2Name} never started"
             Stop-VM $vm2Name -ComputerName $hvServer -force | out-null
-            return $False
+            return $false
         }
 
-    "Info: Succesfully started dependency VM ${vm2Name}"
+        "Info: Succesfully started dependency VM ${vm2Name}"
     }
 
+    #
     # Configure NFS for kdump
+    #
     $retVal = SendCommandToVM $vm2ipv4 $sshKey "cd /root && dos2unix kdump_nfs_config.sh && chmod u+x kdump_nfs_config.sh && ./kdump_nfs_config.sh"
-    if ($retVal -eq $False)
+    if ($retVal -eq $false)
     {
         Write-Output "Error: Failed to configure the NFS server!"
         return $false
@@ -141,37 +165,43 @@ if ($vm2Name -And $use_nfs -eq "yes")
 # Configure kdump on the VM
 #
 $retVal = SendCommandToVM $ipv4 $sshKey "cd /root && dos2unix kdump_config.sh && chmod u+x kdump_config.sh && ./kdump_config.sh $crashkernel $vm2ipv4"
-if ($retVal -eq $False)
+if ($retVal -eq $false)
 {
     Write-Output "Error: Failed to configure kdump. Check logs for details."
+    bin\pscp -q -i ssh\${sshKey} root@${ipv4}:summary.log $logdir/${TC_COVERED}_config_fail_summary.log
     return $false
 }
+bin\pscp -q -i ssh\${sshKey} root@${ipv4}:summary.log $logdir/${TC_COVERED}_config_pass_summary.log
+
 #
 # Rebooting the VM in order to apply the kdump settings
 #
-
 $retVal = SendCommandToVM $ipv4 $sshKey "reboot"
 Write-Output "Rebooting the VM."
 
 #
 # Waiting the VM to start up
+#
 Write-Output "Waiting the VM to have a connection..."
 do {
     sleep 5
 } until(Test-NetConnection $ipv4 -Port 22 -WarningAction SilentlyContinue | ? { $_.TcpTestSucceeded } )
 
 #
-# Triger the kernel panic
+# Prepare the kdump related
 #
 $retVal = SendCommandToVM $ipv4 $sshKey "cd /root && dos2unix kdump_execute.sh && chmod u+x kdump_execute.sh && ./kdump_execute.sh"
-if ($retVal -eq $False)
+if ($retVal -eq $false)
 {
     Write-Output "Error: Configuration is not correct. Check logs for details."
-    bin\pscp -q -i ssh\${sshKey} root@${ipv4}:summary.log $logdir
+    bin\pscp -q -i ssh\${sshKey} root@${ipv4}:summary.log $logdir/${TC_COVERED}_execute_fail_summary.log
     return $false
 }
+bin\pscp -q -i ssh\${sshKey} root@${ipv4}:summary.log $logdir/${TC_COVERED}_execute_passsummary.log
 
+#
 # Trigger the kernel panic
+#
 Write-Output "Trigger the kernel panic..."
 if ($nmi -eq 1){
     # Waiting to kdump_execute.sh to finish his activity.
@@ -196,38 +226,46 @@ Start-Sleep -S 200
 if ((Get-VMIntegrationService -VMName $vmName -ComputerName $hvServer | ?{$_.name -eq "Heartbeat"}).PrimaryStatusDescription -eq "Lost Communication") {
     Write-Output "Error : Lost Communication to VM"
     Stop-VM -Name $vmName -ComputerName $hvServer -Force
-    return $False
+    return $false
 }
-
 Write-Output "Info: VM Heartbeat is OK"
 
+#
 # Waiting the VM to have a connection
+#
 Write-Output "Checking the VM connection after kernel panic..."
-
 $sts = WaitForVMToStartSSH $ipv4 100
 if (-not $sts[-1]){
     Write-Output "Error: $vmName didn't restart after triggering the crash"
     return $false
 }
-
 Write-Output "Connection to VM is good. Checking the results..."
 
 #
 # Verifying if the kernel panic process creates a vmcore file of size 10M+
 #
 $retVal = SendCommandToVM $ipv4 $sshKey "cd /root && dos2unix kdump_results.sh && chmod u+x kdump_results.sh && ./kdump_results.sh $vm2ipv4"
-if ($retVal -eq $False)
+if ($retVal -eq $false)
 {
     Write-Output "Error: Results are not as expected. Check logs for details."
-    bin\pscp -q -i ssh\${sshKey} root@${ipv4}:summary.log $logdir
+    bin\pscp -q -i ssh\${sshKey} root@${ipv4}:summary.log $logdir/${TC_COVERED}_results_summary.log
+    #
+    # Stop NFS server VM
+    #
+    if ($vm2Name) {
+        Stop-VM -vmName $vm2Name -ComputerName $hvServer -Force
+    }
     return $false
 }
-
-bin\pscp -q -i ssh\${sshKey} root@${ipv4}:summary.log $logdir
-
+$result = bin\plink.exe -i ssh\${sshKey} root@${ipv4} "find /var/crash/ -name vmcore -type f -size +10M"
+Write-Host -F Red "PASS: Get vmcore, and result is $result"
+Write-Output "PASS: Get vmcore, and result is $result"
+bin\pscp -q -i ssh\${sshKey} root@${ipv4}:summary.log $logdir/${TC_COVERED}_results_summary.log
+#
 # Stop NFS server VM
+#
 if ($vm2Name) {
     Stop-VM -vmName $vm2Name -ComputerName $hvServer -Force
 }
 
-return $True
+return $true
