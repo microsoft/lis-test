165a2834829b130fa8902e4bcdb43c4f771da0da
Xuemin Li <xuli@redhat.com>
2018-06-19 15:20:14 +0800

    Update ReadKVPData_BasicTest test case to add permission check and lsof check

--- WS2012R2/lisa/remote-scripts/ica/KVP_BasicTest.sh
+++ WS2012R2/lisa/remote-scripts/ica/KVP_BasicTest.sh
@@ -1,107 +1,141 @@
 #!/bin/bash
 
 ################################################################
-# 
 # KVP_BasicTest.sh
-# This script will verify that the KVP daemon is started at the boot of the VM. 
-# This script will install and run the KVP client tool to verify that the KVP
-# pools are created and accessible.
-# Make sure we have kvptool.tar.gz file in Automation\..\lisa\Tools folder 
-# 
+# Description:
+# 1. verify that the KVP Daemon is running
+# 2. run the KVP client tool and verify that the data pools are created and accessible
+# 3. check kvp_pool file permission is 644
+# 4. check kernel version supports hv_kvp
+# 5. check lsof number for kvp after sleep 2 minutes
 ################################################################
-ICA_TESTRUNNING="TestRunning"      # The test is running
-ICA_TESTCOMPLETED="TestCompleted"  # The test completed successfully
-ICA_TESTABORTED="TestAborted"      # Error during setup of test
-ICA_TESTFAILED="TestFailed"        # Error while performing the test
-
-CONSTANTS_FILE="constants.sh"
-
-LogMsg()
+InstallLsof()
 {
-    echo `date "+%a %b %d %T %Y"` : ${1}    # To add the timestamp to the log file
+    case $DISTRO in
+
+        redhat*|centos*|fedora*)
+            yum install lsof -y
+            ;;
+        ubuntu* )
+            apt-get install -y lsof
+            ;;
+        suse* )
+            zypper install -y lsof
+                ;;
+        *)
+            msg="ERROR: Distro '$DISTRO' not supported"
+            LogMsg "${msg}"
+            UpdateSummary "${msg}"
+            SetTestStateFailed
+            exit 1
+            ;;
+    esac
+
+    if [ $? -ne 0 ]; then
+        LogMsg "ERROR: Failed to install lsof"
+        UpdateSummary "ERROR: Failed to install lsof"
+        SetTestStateAborted
+        exit 1
+    fi
 }
 
-cd ~
-
-UpdateTestState()
-{
-    echo $1 > $HOME/state.txt
+dos2unix utils.sh
+# Source utils.sh
+. utils.sh || {
+    echo "Error: unable to source utils.sh!"
+    exit 1
 }
-if [ -e ~/summary.log ]; then
-    LogMsg "Cleaning up previous copies of summary.log"
-    rm -rf ~/summary.log
-fi
-
-LogMsg "Updating test case state to running"
-UpdateTestState $ICA_TESTRUNNING
-
-# Source the constants file
-if [ -e ~/${CONSTANTS_FILE} ]; then
-    source ~/${CONSTANTS_FILE}
-else
-    msg="Error: no ${CONSTANTS_FILE} file"
-    echo $msg
-    echo $msg >> ~/summary.log
-    UpdateTestState $ICA_TESTABORTED
-    exit 10
-fi
-
-
 #
-# Make sure constants.sh contains the variables we expect
+# Source constants file and initialize most common variables
 #
-if [ "${TC_COVERED:-UNDEFINED}" = "UNDEFINED" ]; then
-    msg="The test parameter TC_COVERED is not defined in ${CONSTANTS_FILE}"
-    echo $msg
-    echo $msg >> ~/summary.log
-    UpdateTestState $ICA_TESTABORTED
-    exit 30
-fi
-
+UtilsInit
 #
-# Echo TCs we cover
+# 1. verify that the KVP Daemon is running
 #
-echo "Covers ${TC_COVERED}" > ~/summary.log
-
-#
-# Verify that the KVP Daemon is running
-#
-pgrep -lf "hypervkvpd|hv_kvp_daemon"
+pid=`pgrep "hypervkvpd|hv_kvp_daemon"`
 if [ $? -ne 0 ]; then
 	LogMsg "KVP Daemon is not running by default"
-	echo "KVP daemon not running, basic test: Failed" >> ~/summary.log
-	UpdateTestState $ICA_TESTFAILED
+	UpdateSummary "KVP daemon not running by default, basic test: Failed"
+	SetTestStateFailed
 	exit 10
-fi	
+fi
 LogMsg "KVP Daemon is started on boot and it is running"
 
 #
-# Extract and install the KVP client tool.
+# 2. run the KVP client tool and verify that the data pools are created and accessible
 #
-mkdir kvptool
-tar -xvf kvp*.gz -C kvptool
-if [ $? -ne 0 ]; then
-	LogMsg "Failed to extract the KVP tool tar file"
-	echo "Installing KVP tool: Failed" >> ~/summary.log
-	UpdateTestState $ICA_TESTFAILED
+uname -a | grep x86_64
+if [ $? -eq 0 ]; then
+    LogMsg "64 bit architecture was detected"
+    kvp_client="kvp_client64"
+else
+    uname -a | grep i686
+    if [ $? -eq 0 ]; then
+        LogMsg "32 bit architecture was detected"
+        kvp_client="kvp_client32"
+    else
+        LogMsg "Error: Unable to detect OS architecture"
+        SetTestStateAborted
+        exit 60
+    fi
+fi
+
+chmod +x /root/kvp_client*
+poolCount=`/root/$kvp_client | grep -i pool | wc -l`
+if [ $poolCount -ne 5 ]; then
+	LogMsg "pools are not created properly"
+	UpdateSummary "Pools are not listed properly, KVP Basic test: Failed"
+	SetTestStateFailed
 	exit 10
 fi
-gcc -o kvptool/kvp_client kvptool/kvp_client.c
-mv ~/kvptool/kvp_client ~/
-chmod 755 ~/kvp_client
+LogMsg "Verified that the 0-4 all the 5 data pools are listed properly"
 
 #
-# Run the KVP client tool and verify that the data pools are created and accessible
+# 3. check kvp_pool file permission is 644
 #
-poolcount="`~/kvp_client | grep Pool | wc -l`"
-if [ $poolcount -ne 5 ]; then
-	LogMsg "pools are not created properly"
-	echo "Pools are not listed properly, KVP Basic test: Failed" >> ~/summary.log
-	UpdateTestState $ICA_TESTFAILED
-	exit 10
+permCount=`stat -c %a /var/lib/hyperv/.kvp_pool* | grep 644 | wc -l`
+if [ $permCount -ne 5 ]; then
+        LogMsg ".kvp_pool file permission is incorrect "
+    	UpdateSummary ".kvp_pool file permission is incorrect"
+    	SetTestStateFailed
+        exit 10
 fi
-LogMsg "Verified that the 0-4 all the 5 data pools are listed properly"  
-echo "KVP Daemon is running and data pools are listed -KVP Basic test : Passed" >>  ~/summary.log
-LogMsg "Updating test case state to completed"
-UpdateTestState $ICA_TESTCOMPLETED
+LogMsg "Verified that .kvp_pool file permission is 644"
+
+#
+# 4. check kernel version supports hv_kvp
+#
+CheckVMFeatureSupportStatus "3.10.0-514"
+if [ $? -eq 0 ]; then
+    ls -la /proc/$pid/fd | grep /dev/vmbus/hv_kvp
+    if [ $? -ne 0 ]; then
+        LogMsg "ERROR: there is no hv_kvp in the /proc/$pid/fd "
+        UpdateSummary "ERROR: there is no hv_kvp in the /proc/$pid/fd"
+        SetTestStateFailed
+        exit 1
+    fi
+else
+    LogMsg "This kernel version does not support /dev/vmbus/hv_kvp, skip this step"
+fi
+
+#
+# 5. check lsof number for kvp after sleep 2 minutes
+#
+GetDistro
+InstallLsof
+
+lsofCountBegin=`lsof | grep -c kvp`
+sleep 120
+lsofCountEnd=`lsof | grep -c kvp`
+if [ $lsofCountBegin -ne $lsofCountEnd ]; then
+        msg="ERROR: kvp file number has changed from $lsofCountBegin to $lsofCountEnd"
+        LogMsg "${msg}"
+        UpdateSummary "${msg}"
+    	SetTestStateFailed
+        exit 10
+fi
+LogMsg "Verified that lsof for kvp is $lsofCountBegin, after 2 minutes is $lsofCountEnd"
+
+UpdateSummary "KVP Daemon running, correct data pools permissions and reasonable lsof number of kvp"
+SetTestStateCompleted
 exit 0
