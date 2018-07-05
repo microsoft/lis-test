5b3ceeaa50ee0eca672a34d8a7c32621f05fb072
Chris Valean <cvalean@cloudbasesolutions.com>
2018-07-05 09:29:08 +0300

    Update Configure-Numa-Rss.ps1

--- WS2012R2/lisa/Infrastructure/Configure-Numa-Rss.ps1
+++ WS2012R2/lisa/Infrastructure/Configure-Numa-Rss.ps1
@@ -20,38 +20,36 @@ param (
     [parameter(Mandatory=$true)]
     [String] $hvServer2,
     [parameter(Mandatory=$false)]
-    [String] $Adapter = "SLOT*"
+    [String] $Adapter = "SLOT*",
     [parameter(Mandatory=$false)]
     [String] $VMname = "dummy"
-   
 )
 
 Get-WmiObject Win32_PnPSignedDriver | Select-Object DeviceName, DriverVersion | Where-Object {$_.DeviceName -like "*Mellanox*"} | Select-Object -First 1 | Format-List
 Enable-NetAdapterRss –Name "$Adapter"
 Set-NetAdapterRss –Name "$Adapter" –Profile NUMAStatic -NumaNode 0 –BaseProcessorNumber $BaseProcessorNumber –MaxProcessorNumber $MaxProcessorNumber –MaxProcessors 8 -NumberOfReceiveQueues 8
-Get-NetAdapterRss -Name "$Adapter"
+
 Get-VM –ComputerName $hvServer1 | Where-Object {$_.State –eq 'Running'} | Stop-VM -TurnOff
 Start-VM -Name $VMname –ComputerName $hvServer1
-$numa = ((get-counter -ListSet "Hyper-V VM Vid Partition" –ComputerName $hvServer1).PathsWithInstances | Where-Object {$_ -like "*dummy*preferred numa node index*"} | get-counter).CounterSamples.CookedValue
-  if ($numa -eq 0)
-  {
-    Write-Host "$VMname VM from $hvServer1 runs on NUMA NODE: $numa; keeping VM up to ensure test VM starts on numa node #1"
-  }
-  else
-  {
-    Stop-VM -Name $VMname –ComputerName $hvServer1 -TurnOff
-  }
-Get-VM –ComputerName $hvServer2 | Where-Object {$_.State –eq 'Running'} | Stop-VM -TurnOff 
-Start-VM -Name $VMname –ComputerName $hvServer2
-$numa = ((get-counter -ListSet "Hyper-V VM Vid Partition" –ComputerName $hvServer2).PathsWithInstances | Where-Object {$_ -like "*dummy*preferred numa node index*"} | get-counter).CounterSamples.CookedValue
-  if ($numa -eq 0)
-  {
-    Write-Host "$VMname VM from $hvServer2 runs on NUMA NODE: $numa; keeping VM up to ensure test VM starts on numa node #1"
-  }
-  else
-  {
-    Stop-VM -Name $VMname –ComputerName $hvServer2 -TurnOff
-  }
-Get-NetAdapter -Name "*SRIOV*"  | Disable-NetAdapter –Confirm:$false
-Get-NetAdapter -Name "*SRIOV*"  | Enable-NetAdapter –Confirm:$false 
 
+$numa = ((get-counter -ListSet "Hyper-V VM Vid Partition" –ComputerName $hvServer1).PathsWithInstances | Where-Object {$_ -like "*dummy*preferred numa node index*"} | get-counter).CounterSamples.CookedValue
+if ($numa -eq 0) {
+    Write-Host "$VMname VM from $hvServer1 runs on NUMA NODE: $numa; keeping VM up to ensure test VM starts on numa node #1"
+}
+else {
+    Stop-VM -Name $VMname –ComputerName $hvServer1 -TurnOff
+}
+
+Get-VM –ComputerName $hvServer2 | Where-Object {$_.State –eq 'Running'} | Stop-VM -TurnOff
+Start-VM -Name $VMname –ComputerName $hvServer2
+
+$numa = ((get-counter -ListSet "Hyper-V VM Vid Partition" –ComputerName $hvServer2).PathsWithInstances | Where-Object {$_ -like "*dummy*preferred numa node index*"} | get-counter).CounterSamples.CookedValue
+if ($numa -eq 0) {
+    Write-Host "$VMname VM from $hvServer2 runs on NUMA NODE: $numa; keeping VM up to ensure test VM starts on numa node #1"
+}
+else {
+    Stop-VM -Name $VMname –ComputerName $hvServer2 -TurnOff
+}
+
+Get-NetAdapter -Name "*SRIOV*"  | Disable-NetAdapter –Confirm:$false
+Get-NetAdapter -Name "*SRIOV*"  | Enable-NetAdapter –Confirm:$false
